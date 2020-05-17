52a5189a0fde0f20d01557d4cf8a19a96e644350
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-05-17 21:47:00 +0300

    ResistorColorDuo - Solved.

--- java/resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ java/resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,5 +1,15 @@
+import java.util.stream.Stream;
+
 class ResistorColorDuo {
+    enum Colors {BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE}
+
     int value(String[] colors) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return Stream.of(colors)
+                .limit(2)
+                .map(String::toUpperCase)
+                .map(Colors::valueOf)
+                .map(Colors::ordinal)
+                .reduce((a, b) -> a * 10 + b)
+                .orElse(0);
     }
 }
--- java/resistor-color-duo/src/test/java/ResistorColorDuoTest.java
+++ java/resistor-color-duo/src/test/java/ResistorColorDuoTest.java
@@ -1,6 +1,5 @@
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Ignore;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,47 +13,52 @@ public class ResistorColorDuoTest {
 
     @Test
     public void testBrownAndBlack() {
-        String[] input = { "brown", "black" };
+        String[] input = {"brown", "black"};
         int expected = 10;
         int actual = resistorColorDuo.value(input);
 
         assertEquals(expected, actual);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testBlueAndGrey() {
-        String[] input = { "blue", "grey" };
+        String[] input = {"blue", "grey"};
         int expected = 68;
         int actual = resistorColorDuo.value(input);
 
         assertEquals(expected, actual);
     }
 
-    @Ignore("Remove to run test")
+    @Test
+    public void testBlue() {
+        String[] input = {"blue"};
+        int expected = 6;
+        int actual = resistorColorDuo.value(input);
+
+        assertEquals(expected, actual);
+    }
+
     @Test
     public void testYellowAndViolet() {
-        String[] input = { "yellow", "violet" };
+        String[] input = {"yellow", "violet"};
         int expected = 47;
         int actual = resistorColorDuo.value(input);
 
         assertEquals(expected, actual);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testOrangeAndOrange() {
-        String[] input = { "orange", "orange" };
+        String[] input = {"orange", "orange"};
         int expected = 33;
         int actual = resistorColorDuo.value(input);
 
         assertEquals(expected, actual);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testIgnoreAdditionalColors() {
-        String[] input = { "green", "brown", "orange" };
+        String[] input = {"green", "brown", "orange"};
         int expected = 51;
         int actual = resistorColorDuo.value(input);
 
