db638cf4c044040fb9d534e4f981e3377de92e6d
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-05-17 20:59:00 +0300

    Refactoring.

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,11 +1,11 @@
 import java.util.regex.Pattern;
 
 class IsogramChecker {
-    private static final Pattern IS_REPEATING_LETTER
-            = Pattern.compile("(\\pL).*\\1", Pattern.CASE_INSENSITIVE);
+    private static final Pattern REPEATING_LETTER =
+            Pattern.compile("(\\pL).*\\1", Pattern.CASE_INSENSITIVE);
 
     boolean isIsogram(String phrase) {
-        return !IS_REPEATING_LETTER.matcher(phrase).find();
+        return !REPEATING_LETTER.matcher(phrase).find();
     }
 
 }
