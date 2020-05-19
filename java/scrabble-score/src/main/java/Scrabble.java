1cedd0cda550417df2803d2340eff7ce1d6f4665
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-05-19 10:45:28 +0300

    Scrabble solved

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,16 @@
 class Scrabble {
+    private static final int[] LETTER_VALUES =
+            {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    //       A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P   Q  R  S  T  U  V  W  X  Y   Z
+
+    private final String word;
 
     Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.word = word.toUpperCase();
     }
 
     int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return word.chars().map(i -> LETTER_VALUES[i - 'A']).sum();
     }
 
 }
