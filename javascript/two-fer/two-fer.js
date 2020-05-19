470be4fd135d874c4174a6cb85ab5c9f5c6a7ec2
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-05-19 12:38:31 +0300

    Code refactored by mentor suggession

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,3 +1,3 @@
-export const twoFer = (name) => {
-  return `One for ${ name == null ? 'you' : name }, one for me.`;
+export const twoFer = (name = 'you') => {
+  return `One for ${ name }, one for me.`;
 };
