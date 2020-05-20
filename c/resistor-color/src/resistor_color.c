70f1577c78b79d5e6dca3a75f23714715348137b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-05-20 20:25:02 +0300

    Solved: resistor_color.c

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -1,1 +1,15 @@
 #include "resistor_color.h"
+
+int color_code(resistor_band_t color) {
+    return (int) color;
+}
+
+
+const resistor_band_t* colors() {
+    static resistor_band_t result[] = {
+            BLACK, BROWN, RED, ORANGE, YELLOW,
+            GREEN, BLUE, VIOLET, GREY, WHITE
+    };
+
+    return result;
+}
--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -2,7 +2,9 @@
 #define RESISTOR_COLOR_H
 
 typedef enum {
-
+    BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE
 } resistor_band_t;
 
+int color_code(resistor_band_t);
+const resistor_band_t* colors(void);
 #endif
