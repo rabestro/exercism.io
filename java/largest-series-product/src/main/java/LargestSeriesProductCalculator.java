4d815a97d72a5e574de995a90c3bb93cf07bce67
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-05-20 05:51:16 +0300

    LargestSeriesProductCalculator solved

--- java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
+++ java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
@@ -1,9 +1,33 @@
+import java.util.Arrays;
+
+import static java.util.stream.IntStream.rangeClosed;
+
 class LargestSeriesProductCalculator {
+    private final int[] digits;
+
     LargestSeriesProductCalculator(String inputNumber) {
-        throw new UnsupportedOperationException ("Delete this statement and write your own implementation.");
+        digits = inputNumber.chars().map(digit -> {
+            if (digit < '0' || digit > '9') {
+                throw new IllegalArgumentException("String to search may only contain digits.");
+            }
+            return digit - '0';
+        }).toArray();
     }
 
     long calculateLargestProductForSeriesLength(int numberOfDigits) {
-        throw new UnsupportedOperationException ("Delete this statement and write your own implementation.");
+        if (numberOfDigits < 0) {
+            throw new IllegalArgumentException("Series length must be non-negative.");
+        }
+        if (numberOfDigits > digits.length) {
+            throw new IllegalArgumentException(
+                    "Series length must be less than or equal to the length of the string to search.");
+        }
+        return rangeClosed(0, digits.length - numberOfDigits)
+                .mapToLong(start -> Arrays.stream(digits, start, start + numberOfDigits)
+                        .reduce((a, b) -> a * b)
+                        .orElse(1))
+                .max()
+                .orElse(1);
     }
+
 }
