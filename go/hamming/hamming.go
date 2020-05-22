db5bdbd03333fd8f1becf03e40aa6ae87d0f834d
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-05-23 00:17:48 +0300

    Added documentation for function Distance in hamming.go

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -2,6 +2,11 @@ package hamming
 
 import "errors"
 
+/* Calculate the Hamming Distance between two DNA strands.
+
+The Hamming distance is only defined for sequences of equal length, so an
+attempt to calculate it between sequences of different lengths returns error.
+*/
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
 		return 0, errors.New("the sequences have to be equal length")
