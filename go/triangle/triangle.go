a245b76c4f8b3ef3bca57972827cf12695487f56
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-05-24 15:53:05 +0300

    Added solution for Triangle

---
+++ go/triangle/triangle.go
@@ -0,0 +1,31 @@
+// Package triangle contains solution to Triangle exercise
+package triangle
+
+import "math"
+
+type Kind int
+
+const (
+	NaT Kind = iota // not a triangle
+	Equ             // equilateral
+	Iso             // isosceles
+	Sca             // scalene
+)
+
+// KindFromSides determines if a triangle is equilateral, isosceles, or scalene.
+func KindFromSides(a, b, c float64) Kind {
+	var k Kind
+	switch {
+	case math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c):
+	case math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0):
+	case a+b < c || a+c < b || b+c < a || a <= 0 || b <= 0 || c <= 0:
+		k = NaT
+	case a == b && b == c:
+		k = Equ
+	case a == b || a == c || b == c:
+		k = Iso
+	default:
+		k = Sca
+	}
+	return k
+}
