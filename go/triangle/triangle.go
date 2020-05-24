11a9513c6cd41bd2448443450253069d7f3544ff
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-05-24 15:59:25 +0300

    Refactoring for triangle.go

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -15,10 +15,12 @@ const (
 // KindFromSides determines if a triangle is equilateral, isosceles, or scalene.
 func KindFromSides(a, b, c float64) Kind {
 	var k Kind
+
 	switch {
 	case math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c):
 	case math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0):
-	case a+b < c || a+c < b || b+c < a || a <= 0 || b <= 0 || c <= 0:
+	case a <= 0 || b <= 0 || c <= 0:
+	case a+b < c || a+c < b || b+c < a:
 		k = NaT
 	case a == b && b == c:
 		k = Equ
