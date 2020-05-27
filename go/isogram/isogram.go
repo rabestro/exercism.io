a6d95f822dcc954f5da413c681f65735c3517647
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-05-27 14:09:58 +0300

    Refactoring isogram.go

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -10,10 +10,10 @@ func IsIsogram(word string) bool {
 	var bitset uint32 = 0
 	for _, c := range strings.ToLower(word) {
 		if unicode.IsLetter(c) {
-			if index := c - 'a'; bitset&(1<<index) != 0 {
-				return false
-			} else {
+			if index := c - 'a'; bitset&(1<<index) == 0 {
 				bitset |= 1 << index
+			} else {
+				return false
 			}
 		}
 	}
