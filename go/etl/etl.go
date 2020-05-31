6452f6f7e032bede0b77a267cd7d163e4feb4870
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-05-31 22:10:56 +0300

    Refactoring Transform

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -3,12 +3,12 @@ package etl
 import "strings"
 
 // Transform transforms the legacy data format to the shiny new format.
-func Transform(m map[int][]string) map[string]int {
-	result := make(map[string]int)
-	for v := range m {
-		for _, c := range m[v] {
-			result[strings.ToLower(c)] = v
+func Transform(oldFormat map[int][]string) map[string]int {
+	newFormat := make(map[string]int)
+	for score, letters := range oldFormat {
+		for _, letter := range letters {
+			newFormat[strings.ToLower(letter)] = score
 		}
 	}
-	return result
+	return newFormat
 }
