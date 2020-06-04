e83b2731aed4aab4ae9b7031de0e9d7f7ef21a53
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-06-04 23:21:50 +0300

    Changed solution for Luhn

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -8,21 +8,21 @@ import (
 // Valid determines whether or not a number is valid per the Luhn formula.
 func Valid(cardNumber string) bool {
 	number := strings.ReplaceAll(cardNumber, " ", "")
-	if _, err := strconv.Atoi(number); err == nil && len(number) > 1 {
-		sum := 0
-		isEven := len(number)%2 == 0
-		for _, symbol := range number {
-			digit := symbol - '0'
-			if isEven {
-				digit *= 2
-				if digit > 9 {
-					digit -= 9
-				}
-			}
-			isEven = !isEven
-			sum += int(digit)
-		}
-		return sum%10 == 0
+	if _, err := strconv.Atoi(number); err != nil || len(number) < 2 {
+		return false
 	}
-	return false
+	sum := 0
+	isEven := len(number)%2 == 0
+	for _, symbol := range number {
+		digit := symbol - '0'
+		if isEven {
+			digit *= 2
+			if digit > 9 {
+				digit -= 9
+			}
+		}
+		isEven = !isEven
+		sum += int(digit)
+	}
+	return sum%10 == 0
 }
