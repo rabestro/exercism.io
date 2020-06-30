3fdb59bd00b0ceaf561667ae4ab8abe24855ca85
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-06-30 21:01:12 +0300

    Added solution for Armstrong Numbers

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,15 @@
 class ArmstrongNumbers {
 
     boolean isArmstrongNumber(int numberToCheck) {
-
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-
+        for (int i = numberToCheck, pow = (i + "").length(); i > 0; i /= 10) {
+            final int digit = i % 10;
+            int power = 1;
+            for (int j = 0; j < pow; j++) {
+                power *= digit;
+            }
+            numberToCheck -= power;
+        }
+        return 0 == numberToCheck;
     }
 
 }
