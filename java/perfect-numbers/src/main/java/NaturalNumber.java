db99357e21c3575c2ca7257a897faa49406754ee
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-14 19:45:59 +0300

    Added minimum implementation to pass first test

--- java/perfect-numbers/src/main/java/NaturalNumber.java
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,5 +1,11 @@
 class NaturalNumber {
+    private final int number;
 
+    public NaturalNumber(final int number) {
+        this.number = number;
+    }
 
-
+    public Classification getClassification() {
+        return Classification.PERFECT;
+    }
 }
