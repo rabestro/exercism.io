9a89932623a4c8dc7ed81b85df1b7838859ce849
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-14 20:02:17 +0300

    Added solution for the problem

--- java/perfect-numbers/src/main/java/NaturalNumber.java
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,11 +1,29 @@
-class NaturalNumber {
+import java.util.stream.IntStream;
+
+import static java.util.stream.IntStream.rangeClosed;
+
+final class NaturalNumber {
     private final int number;
 
     public NaturalNumber(final int number) {
+        if (number < 1) {
+            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+        }
         this.number = number;
     }
 
+    public IntStream divisors() {
+        return rangeClosed(1, number / 2).filter(i -> number % i == 0);
+    }
+
     public Classification getClassification() {
+        final var sum = divisors().sum();
+        if (sum < number) {
+            return Classification.DEFICIENT;
+        }
+        if (sum > number) {
+            return Classification.ABUNDANT;
+        }
         return Classification.PERFECT;
     }
 }
