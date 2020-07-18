509e2c6e2b42dc51d6eb6cd79194e265daab0f8c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-18 23:22:01 +0300

    Added empty method withNumbers

--- java/minesweeper/src/main/java/MinesweeperBoard.java
+++ java/minesweeper/src/main/java/MinesweeperBoard.java
@@ -1,3 +1,14 @@
-public class MinesweeperBoard {
+import java.util.Collections;
+import java.util.List;
 
+public class MinesweeperBoard {
+    private final List<String> inputBoard;
+
+    public MinesweeperBoard(List<String> inputBoard) {
+        this.inputBoard = inputBoard;
+    }
+
+    public List<String> withNumbers() {
+        return Collections.emptyList();
+    }
 }
