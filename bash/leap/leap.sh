e6f97e7d82e94ba2ec3fc4c2847fc81a41d84a0a
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-23 20:45:00 +0300

    Improved solution for the problem "Leap"

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-if [ $(( $1%4 )) -eq 0 ]; then
+if [ "$#" -ne 1 ]; then
+  echo "Usage: leap.sh <year>"
+  exit 1
+fi
+
+if [[ $(( $1%4 )) -eq 0 && $(( $1%100 )) -ne 0 || $(( $1%400 )) -eq 0 ]]; then
   echo true
 else
   echo false
