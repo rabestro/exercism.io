0b8eab1dc29fb60a9296dbbff1c57b986261ea03
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-23 22:18:17 +0300

    An incomplete solution added for "Resistor Color Duo"

--- bash/resistor-color-duo/resistor_color_duo.sh
+++ bash/resistor-color-duo/resistor_color_duo.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+declare -A colors=( ["black"]=0 ["brown"]=1 ["red"]=2 ["orange"]=3 ["yellow"]=4 ["green"]=5 ["blue"]=6 ["violet"]=7 ["grey"]=8 ["white"]=9 )
+a=${colors[$1]}
+b=${colors[$2]}
+echo $a$b
