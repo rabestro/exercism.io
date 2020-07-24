9cd2b415325856f2c3e411139250b3f91f02c443
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-25 01:02:37 +0300

    Refactoring hamming.sh according to the mentor's recommendations

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-if [[ "$#" -ne 2 ]]; then
+if (( $# != 2 )); then
   echo "Usage: hamming.sh <string1> <string2>"
   exit 1
 fi
 
-first=$1
-second=$2
+declare -r first=$1
+declare -r second=$2
 
-if [[ "${#first}" -ne "${#second}" ]]; then
+if (( ${#first} != ${#second} )); then
   echo "left and right strands must be of equal length"
   exit 1
 fi
 
-difference=0
+declare -i difference=0
 
 for ((i = 0; i < ${#first}; i++)); do
   if [[ ${first:$i:1} != "${second:$i:1}" ]]; then
-    difference=$((difference + 1))
+    ((difference++))
   fi
 done
 
-echo $difference
+echo "$difference"
