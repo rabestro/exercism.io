287f62089bc4da9d0bb53ba5dc1e93553812640a
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-24 18:58:00 +0300

    Added problem and first step of solution for "Resistor Color Trio"

---
+++ bash/resistor-color-trio/resistor_color_trio.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+declare -A colors=(["black"]=0 ["brown"]=1 ["red"]=2 ["orange"]=3 ["yellow"]=4
+  ["green"]=5 ["blue"]=6 ["violet"]=7 ["grey"]=8 ["white"]=9)
+
+a=${colors[$1]}
+
+if [ -z "$a" ]; then
+  echo "invalid color"
+  exit 1
+fi
+
+b=${colors[$2]}
+
+echo "$a$b ohms"
