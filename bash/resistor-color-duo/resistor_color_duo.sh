151fb1005f17102b643d825602c4e1993536f082
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-24 19:18:33 +0300

    Code reformatted according to the mentor recommendations

--- bash/resistor-color-duo/resistor_color_duo.sh
+++ bash/resistor-color-duo/resistor_color_duo.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-declare -A colors=(["black"]=0 ["brown"]=1 ["red"]=2 ["orange"]=3 ["yellow"]=4
-  ["green"]=5 ["blue"]=6 ["violet"]=7 ["grey"]=8 ["white"]=9)
+declare -A colors=(
+  ["black"]=0 ["brown"]=1 ["red"]=2 ["orange"]=3 ["yellow"]=4
+  ["green"]=5 ["blue"]=6 ["violet"]=7 ["grey"]=8 ["white"]=9
+)
 
 a=${colors[$1]}
 
-if [ -z "$a" ]; then
+if [[ -z $a ]]; then
   echo "invalid color"
   exit 1
 fi
 
 b=${colors[$2]}
+
+if [[ -z $b ]]; then
+  echo "invalid color"
+  exit 1
+fi
 
 echo "$a$b"
