9e2be1aea2ca3652c582bd3d3864a5739bb51b2f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-25 15:55:34 +0300

    Refactoring, issue #4

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+phrase=$1
+re="([a-zA-z])[a-zA-Z']*(.*)"
+echo "Phrase: $phrase"
+echo "RegExp: $re"
+acronym=""
+[[ $phrase =~ $re ]] && echo true
+
+while [[ $phrase =~ $re ]]; do
+  acronym=${acronym}${BASH_REMATCH[1]}
+  phrase=${BASH_REMATCH[2]}
+  echo "Phrase: $phrase Acronym: $acronym"
+done
+
+echo "$acronym"
--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if (( $# != 2 )); then
+if (($# != 2)); then
   echo "Usage: hamming.sh <string1> <string2>"
   exit 1
 fi
@@ -8,7 +8,7 @@ fi
 declare -r first=$1
 declare -r second=$2
 
-if (( ${#first} != ${#second} )); then
+if ((${#first} != ${#second})); then
   echo "left and right strands must be of equal length"
   exit 1
 fi
@@ -16,9 +16,7 @@ fi
 declare -i difference=0
 
 for ((i = 0; i < ${#first}; i++)); do
-  if [[ ${first:$i:1} != "${second:$i:1}" ]]; then
-    ((difference++))
-  fi
+  [[ ${first:$i:1} != "${second:$i:1}" ]] && ((difference++))
 done
 
 echo "$difference"
