fdf8ddbf91249f8c14b999b6106c744d52f278d2
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-28 23:44:32 +0300

    Added simple code to pass first tests

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
 
-phrase=$1
-re="([a-zA-z])[a-zA-Z']*(.*)"
-echo "Phrase: $phrase"
-echo "RegExp: $re"
-acronym=""
-[[ $phrase =~ $re ]] && echo true
+echo "$1" | sed 's/\([[:alpha:]]\)[a-zA-Z]*[^a-zA-Z]*/\1/g'
 
-while [[ $phrase =~ $re ]]; do
-  acronym=${acronym}${BASH_REMATCH[1]}
-  phrase=${BASH_REMATCH[2]}
-  echo "Phrase: $phrase Acronym: $acronym"
-done
-
-echo "$acronym"
