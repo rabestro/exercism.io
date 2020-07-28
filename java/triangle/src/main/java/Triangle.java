fcbd24861b4c52b63cde8ae091e23e4a16c538de
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-28 13:50:35 +0300

    Added first step for "Triangle" to pass the very first test

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -1,11 +1,16 @@
 class Triangle {
+    private final double side1;
+    private final double side2;
+    private final double side3;
 
     Triangle(double side1, double side2, double side3) throws TriangleException {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
     }
 
     boolean isEquilateral() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return true;
     }
 
     boolean isIsosceles() {
