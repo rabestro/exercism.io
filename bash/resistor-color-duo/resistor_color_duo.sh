18cda86a939098d778f6ab5184530f1139584cfc
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-28 23:34:52 +0300

    Code reformatted according to the mentor recommendations

--- bash/resistor-color-duo/resistor_color_duo.sh
+++ bash/resistor-color-duo/resistor_color_duo.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
+declare -Ar COLORS=(
+  ["black"]=0 ["brown"]=1 ["red"]=2 ["orange"]=3 ["yellow"]=4
+  ["green"]=5 ["blue"]=6 ["violet"]=7 ["grey"]=8 ["white"]=9
+)
+
 color_number() {
-  declare -A colors=(
-    ["black"]=0 ["brown"]=1 ["red"]=2 ["orange"]=3 ["yellow"]=4
-    ["green"]=5 ["blue"]=6 ["violet"]=7 ["grey"]=8 ["white"]=9
-  )
-  if [[ -z ${colors[$1]} ]]; then
+  if [[ -z ${COLORS[$1]} ]]; then
     echo "invalid color" >&2
     return 1
   fi
-  echo "${colors[$1]}"
+  echo "${COLORS[$1]}"
 }
 
 a=$(color_number "$1") || exit 1
