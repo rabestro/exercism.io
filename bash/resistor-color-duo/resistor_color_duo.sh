2f7df8992cb9df0a155c3cd916b661f16a25f89f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-28 21:30:55 +0300

    Code reformatted according to the mentor recommendations

--- bash/resistor-color-duo/resistor_color_duo.sh
+++ bash/resistor-color-duo/resistor_color_duo.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
 
-trap "exit 1" TERM
-export TOP_PID=$$
-
 color_number() {
   declare -A colors=(
     ["black"]=0 ["brown"]=1 ["red"]=2 ["orange"]=3 ["yellow"]=4
     ["green"]=5 ["blue"]=6 ["violet"]=7 ["grey"]=8 ["white"]=9
   )
-
-  number=${colors[$1]}
-
-  if [[ -z $number ]]; then
-    echo "invalid color"
-    kill -s TERM $TOP_PID
+  if [[ -z ${colors[$1]} ]]; then
+    echo "invalid color" >&2
+    return 1
   fi
-  echo "$number"
+  echo "${colors[$1]}"
 }
 
-echo "$(color_number "$1")$(color_number "$2")"
+a=$(color_number "$1") || exit 1
+b=$(color_number "$2") || exit 1
+echo "$a$b"
