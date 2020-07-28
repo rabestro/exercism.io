cdb264cbd9abd5cedb9bfacdf6ac522ccfb2fa2d
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-28 21:15:57 +0300

    Added minimum code to pass next test

--- bash/resistor-color-trio/resistor_color_trio.sh
+++ bash/resistor-color-trio/resistor_color_trio.sh
@@ -7,7 +7,8 @@ validate() {
   fi
 }
 
-declare -A colors=(["black"]=0 ["brown"]=1 ["red"]=2 ["orange"]=3 ["yellow"]=4
+declare -A colors=(
+  ["black"]=0 ["brown"]=1 ["red"]=2 ["orange"]=3 ["yellow"]=4
   ["green"]=5 ["blue"]=6 ["violet"]=7 ["grey"]=8 ["white"]=9)
 
 suffix=(" ohms" "0 ohms" "00 ohms" " kiloohms" "0 kiloohms" "00 kiloohms" " megaohms" "0 megaohms" "00 megaohms")
@@ -22,8 +23,8 @@ c=${colors[$3]}
 validate "$c"
 
 if ((b == 0)); then
-  ((c++))
   unset b
+  ((a == 0)) || ((c++))
 fi
 
 echo "$a$b${suffix[$c]}"
