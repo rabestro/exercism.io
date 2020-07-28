897cd6150ff6c704d5997bdc239ad65e951eca3a
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-28 21:07:50 +0300

    Added minimum code to pass next test

--- bash/resistor-color-trio/resistor_color_trio.sh
+++ bash/resistor-color-trio/resistor_color_trio.sh
@@ -10,7 +10,7 @@ validate() {
 declare -A colors=(["black"]=0 ["brown"]=1 ["red"]=2 ["orange"]=3 ["yellow"]=4
   ["green"]=5 ["blue"]=6 ["violet"]=7 ["grey"]=8 ["white"]=9)
 
-suffix=(" ohms" "0 ohms" "00 ohms" " kiloohms")
+suffix=(" ohms" "0 ohms" "00 ohms" " kiloohms" "0 kiloohms")
 
 a=${colors[$1]}
 validate "$a"
