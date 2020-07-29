ea1bf60fa655f94014df6738ab751d71339c99c2
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-29 15:44:32 +0300

    All tests passed.

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,4 +1,9 @@
-class RotationalCipher {
+import static java.lang.Character.isAlphabetic;
+import static java.lang.Character.isUpperCase;
+
+final class RotationalCipher {
+    private static final int ALPHABET_LETTERS_COUNT = 26;
+
     private final int shiftKey;
 
     RotationalCipher(int shiftKey) {
@@ -6,7 +11,17 @@ class RotationalCipher {
     }
 
     String rotate(String data) {
-        return data;
+        return data.chars()
+                .map(this::encode)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 
+    private int encode(final int symbol) {
+        if (isAlphabetic(symbol)) {
+            final char firstLetter = isUpperCase(symbol) ? 'A' : 'a';
+            return firstLetter + (symbol - firstLetter + shiftKey) % ALPHABET_LETTERS_COUNT;
+        }
+        return symbol;
+    }
 }
