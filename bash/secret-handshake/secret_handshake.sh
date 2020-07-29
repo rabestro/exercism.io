d6c013bc6fd2f7fedb53b533677370f6dc4a5466
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-29 23:28:46 +0300

    Removed optional argument

--- bash/secret-handshake/secret_handshake.sh
+++ bash/secret-handshake/secret_handshake.sh
@@ -3,7 +3,7 @@
 readonly handshake=("wink" "double blink" "close your eyes" "jump")
 readonly code=$1
 
-((code >> 4)) && range=({3..0..-1}) || range=({0..3})
+((code >> 4)) && range=({3..0}) || range=({0..3})
 
 for i in "${range[@]}"; do
   if ((1 & (code >> i))); then
