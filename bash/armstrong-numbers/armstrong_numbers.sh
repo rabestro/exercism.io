ad437843e1fc90e955ade24b55548fa0fecd777b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-30 22:28:03 +0300

    All tests passed.

--- bash/armstrong-numbers/armstrong_numbers.sh
+++ bash/armstrong-numbers/armstrong_numbers.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-echo "true"
+readonly power=${#1}
+readonly number=$1
+
+sum=0
+for digit in $(fold -w1 <<<"$number"); do
+  ((sum += digit ** power))
+done
+
+if ((sum == number)); then
+  echo true
+else
+  echo false
+fi
