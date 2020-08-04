a421d16a6c7f152d69790c0531e6899fd571bbbf
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-04 11:29:39 +0300

    Problem solved

--- java/roman-numerals/src/main/java/RomanNumerals.java
+++ java/roman-numerals/src/main/java/RomanNumerals.java
@@ -1,5 +1,27 @@
 enum RomanNumerals {
+    M(1000), CM(900), D(500), CD(400), C(100), XC(90),
+    L(50), XL(40), X(10), IX(9), V(5), IV(4), I(1);
 
+    private final int value;
+
+    RomanNumerals(final int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static String arabicToRoman(int number) {
+        final var sb = new StringBuilder();
+        for (var romanNumber : RomanNumerals.values()) {
+            while (romanNumber.getValue() <= number) {
+                number -= romanNumber.getValue();
+                sb.append(romanNumber.toString());
+            }
+        }
+        return sb.toString();
+    }
 }
 
 class RomanNumeral {
@@ -10,6 +32,6 @@ class RomanNumeral {
     }
 
     public String getRomanNumeral() {
-        return "I";
+        return RomanNumerals.arabicToRoman(value);
     }
 }
