4b7d3dafe980be59811e28d9715362ca5b59276a
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-04 21:23:47 +0300

    All tests passed.

--- java/luhn/src/main/java/LuhnValidator.java
+++ java/luhn/src/main/java/LuhnValidator.java
@@ -2,7 +2,23 @@ class LuhnValidator {
 
     boolean isValid(String candidate) {
         final var number = candidate.replaceAll(" ", "");
-        return number.matches("[0-9]{3,}");
+        if (!number.matches("[0-9]{2,}")) {
+            return false;
+        }
+        var sum = 0;
+        var isEven = number.length() % 2 == 0;
+        for (var symbol : number.toCharArray()) {
+            var digit = Character.getNumericValue(symbol);
+            if (isEven) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit -= 9;
+                }
+            }
+            sum += digit;
+            isEven = !isEven;
+        }
+        return sum % 10 == 0;
     }
 
 }
--- java/luhn/src/test/java/LuhnValidatorTest.java
+++ java/luhn/src/test/java/LuhnValidatorTest.java
@@ -23,49 +23,41 @@ public class LuhnValidatorTest {
         assertFalse(luhnValidator.isValid("0"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testSimpleValidSINReversedRemainsValid() {
         assertTrue(luhnValidator.isValid("059"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testSimpleValidSINReversedBecomesInvalid() {
         assertTrue(luhnValidator.isValid("59"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testValidCanadianSINValid() {
         assertTrue(luhnValidator.isValid("055 444 285"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testInvalidCanadianSINInvalid() {
         assertFalse(luhnValidator.isValid("055 444 286"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testInvalidCreditCardInvalid() {
         assertFalse(luhnValidator.isValid("8273 1232 7352 0569"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testInvalidLongNumberWithAnEvenRemainder() {
         assertFalse(luhnValidator.isValid("1 2345 6789 1234 5678 9012"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testValidNumberWithAnEvenNumberOfDigits() {
         assertTrue(luhnValidator.isValid("095 245 88"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testValidNumberWithAnOddNumberOfSpaces() {
         assertTrue(luhnValidator.isValid("234 567 891 234"));
@@ -86,32 +78,27 @@ public class LuhnValidatorTest {
         assertFalse(luhnValidator.isValid("055# 444$ 285"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testSingleSpaceWithZeroInvalid() {
         assertFalse(luhnValidator.isValid(" 0"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testMoreThanSingleZeroValid() {
         assertTrue(luhnValidator.isValid("0000 0"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testDigitNineConvertedToOutputNine() {
         assertTrue(luhnValidator.isValid("091"));
     }
 
 
-    @Ignore("Remove to run test")
     @Test
     public void testUsingASCIIValueForNonDoubledNonDigitNotAllowed() {
         assertFalse(luhnValidator.isValid("055b 444 285"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testUsingASCIIValueForDoubledNonDigitNotAllowed() {
         assertFalse(luhnValidator.isValid(":9"));
@@ -122,7 +109,6 @@ public class LuhnValidatorTest {
      * passing implementations. For more detail, check out issue #972 here:
      * (https://github.com/exercism/java/issues/972).
     */
-    @Ignore("Remove to run test")
     @Test
     public void testStringContainingSymbolsInvalidJavaTrackSpecific() {
         assertFalse(luhnValidator.isValid("85&"));
