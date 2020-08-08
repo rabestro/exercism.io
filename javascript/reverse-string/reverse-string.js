f518c17c7a72e0add74afe7b23d265e274512e0c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-08 19:48:40 +0300

    Added comments for the function

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,6 +1,5 @@
 //
-// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
-// convenience to get you started writing code faster.
+// Reverse a string
 //
 
 export const reverseString = (string) => {
