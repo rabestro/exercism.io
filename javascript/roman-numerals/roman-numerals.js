30b36e220580889be735b9471db449563835608e
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-08 15:26:23 +0300

    Added minimum code to pass the next test

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -3,7 +3,7 @@
 //
 
 export const toRoman = (number) => {
-    return ["", "C", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"][Math.floor(number % 1000 / 100)]
+    return ["", "C", "CC", "CCC", "CD", "D", "DC", "LXX", "LXXX", "XC"][Math.floor(number % 1000 / 100)]
         + ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"][Math.floor(number % 100 / 10)]
         + ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"][number % 10];
 };
