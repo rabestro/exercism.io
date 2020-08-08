d308f90769883be442aa50dbc68aec041d6cef4f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-08 11:56:45 +0300

    Solved "Gigasecond"

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,10 @@
 //
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
+// Given a moment, determine the moment that would be after a gigasecond has passed.
+// A gigasecond is 10^9 (1,000,000,000) seconds.
 //
 
-export const gigasecond = () => {
-  return new Date(Date.parse('2043-01-01T01:46:40Z'));
+export const gigasecond = (date) => {
+  const GIGASECOND = 1000000000;
+  const newTime = date.getTime() + GIGASECOND * 1000;
+  return new Date(newTime);
 };
