0fed744783869d347ac4dc32a09d1ef9ab4a44d1
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-09 20:33:45 +0300

    Added minimum code to pass the first test

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -1,11 +1,10 @@
 class Darts {
 
     Darts(double x, double y) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
     int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return 0;
     }
 
 }
