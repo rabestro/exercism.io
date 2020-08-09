e96a890ed00f5363fa7cda56d3fb9d0e03051a81
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-10 00:54:39 +0300

    Added minimum code to pass the next code

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-echo "Whatever."
+readonly sentence=${1//[!a-zA-Z]/}
+
+if [[ $sentence =~ "\?$" ]]; then
+  isQuestion=true
+else
+  isQuestion=false
+fi
+
+if [[ $sentence == "${sentence^^}" ]]; then
+  isUpper=true
+else
+  isUpper=false
+fi
+
+if [[ -z $sentence ]]; then
+  echo "Fine. Be that way!"
+else
+  echo "Whatever."
+fi
