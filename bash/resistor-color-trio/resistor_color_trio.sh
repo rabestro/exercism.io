13cf0b3879483ae32a8372f00c23c9afc2da1d1a
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-09 10:00:35 +0300

    Added readonly modifier

--- bash/resistor-color-trio/resistor_color_trio.sh
+++ bash/resistor-color-trio/resistor_color_trio.sh
@@ -7,11 +7,11 @@ validate() {
   fi
 }
 
-declare -A colors=(
+readonly -A colors=(
   ["black"]=0 ["brown"]=1 ["red"]=2 ["orange"]=3 ["yellow"]=4
   ["green"]=5 ["blue"]=6 ["violet"]=7 ["grey"]=8 ["white"]=9)
 
-suffix=(" " "0 " "00 " " kilo" "0 kilo" "00 kilo" " mega" "0 mega" "00 mega" " giga")
+readonly suffix=(" " "0 " "00 " " kilo" "0 kilo" "00 kilo" " mega" "0 mega" "00 mega" " giga")
 
 a=${colors[$1]}
 validate "$a"
