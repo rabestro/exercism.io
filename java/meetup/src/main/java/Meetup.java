fe71c436f8b80494b34945b07d73453994ec745d
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-10 00:08:48 +0300

    Removed redundant streams

--- java/meetup/src/main/java/Meetup.java
+++ java/meetup/src/main/java/Meetup.java
@@ -1,35 +1,29 @@
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.TemporalAdjusters;
-import java.util.function.UnaryOperator;
-import java.util.stream.Stream;
+import java.time.YearMonth;
 
 class Meetup {
     private final int month;
     private final int year;
+    private final YearMonth yearMonth;
 
     public Meetup(int month, int year) {
         this.month = month;
         this.year = year;
+        yearMonth = YearMonth.of(year, month);
     }
 
-    public LocalDate day(DayOfWeek day, MeetupSchedule teenth) {
-        int startDay = 1;
-        int skipDays = 0;
-        UnaryOperator<LocalDate> nextDate = date -> date.plusDays(1);
+    public LocalDate day(DayOfWeek dayOfWeek, MeetupSchedule meetupSchedule) {
+        LocalDate startDate = yearMonth.atDay(switch (meetupSchedule) {
+            case FIRST -> 1;
+            case SECOND -> 8;
+            case THIRD -> 15;
+            case FOURTH -> 22;
+            case LAST -> yearMonth.lengthOfMonth() - 6;
+            case TEENTH -> 13;
+        });
+        int offset = dayOfWeek.getValue() - startDate.getDayOfWeek().getValue();
+        return startDate.plusDays(Math.floorMod(offset, 7));
 
-        switch (teenth) {
-            case FIRST, SECOND, THIRD, FOURTH -> skipDays = teenth.ordinal();
-            case LAST -> {
-                startDay = LocalDate.of(year, month, 1)
-                        .with(TemporalAdjusters.lastDayOfMonth()).getDayOfMonth();
-                nextDate = date -> date.plusDays(-1);
-            }
-            case TEENTH -> startDay = 13;
-        }
-        return Stream.iterate(LocalDate.of(year, month, startDay), nextDate)
-                .filter(date -> date.getDayOfWeek() == day)
-                .skip(skipDays)
-                .findFirst().orElseThrow();
     }
 }
