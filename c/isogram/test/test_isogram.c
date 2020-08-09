a42e1d8d888a3ada14d4a7b1cce56d4dd044a793
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-09 19:09:15 +0300

    All tests passed. Solved.

--- c/isogram/test/test_isogram.c
+++ c/isogram/test/test_isogram.c
@@ -22,7 +22,6 @@ static void test_null(void)
 
 static void test_lower_case_only(void)
 {
-//   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("isogram"));
 }
 
@@ -34,61 +33,61 @@ static void test_duplicated_letter(void)
 
 static void test_duplicated_letter_from_end_of_alphabet(void)
 {
-   TEST_IGNORE();
+//   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("zzyzx"));
 }
 
 static void test_longest_known_isogram(void)
 {
-   TEST_IGNORE();
+//   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("subdermatoglyphic"));
 }
 
 static void test_duplicated_letter_mixed_case(void)
 {
-   TEST_IGNORE();
+//   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("Alphabet"));
 }
 
 static void test_duplicated_letter_mixed_case_lowercase_first(void)
 {
-   TEST_IGNORE();
+//   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("alphAbet"));
 }
 
 static void test_non_letter_char(void)
 {
-   TEST_IGNORE();
+//   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("thumbscrew-japingly"));
 }
 
 static void test_duplicated_letter_following_non_letter_char(void)
 {
-   TEST_IGNORE();
+//   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("thumbscrew-jappingly"));
 }
 
 static void test_duplicated_non_letter_char(void)
 {
-   TEST_IGNORE();
+//   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("six-year-old"));
 }
 
 static void test_multiple_whitespace(void)
 {
-   TEST_IGNORE();
+//   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("Emily Jung Schwartzkopf"));
 }
 
 static void test_duplicated_letter_within_word(void)
 {
-   TEST_IGNORE();
+//   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("accentor"));
 }
 
 static void test_same_first_and_last_characters(void)
 {
-   TEST_IGNORE();
+//   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("angola"));
 }
 
