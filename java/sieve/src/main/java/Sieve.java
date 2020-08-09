901d5ccfb1426da8dcbc6b9ffd8b6ee42c8620d4
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-09 20:18:51 +0300

    All tests passed. Solved.

--- java/sieve/src/main/java/Sieve.java
+++ java/sieve/src/main/java/Sieve.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 class Sieve {
+    private final List<Integer> primes;
+    private final int maxPrime;
+
     Sieve(int maxPrime) {
-        throw new UnsupportedOperationException("Delete this statement and provide your own implementation.");
+        this.maxPrime = maxPrime;
+        primes = new ArrayList<>();
     }
 
     List<Integer> getPrimes() {
-        throw new UnsupportedOperationException("Delete this statement and provide your own implementation.");
+        IntStream.rangeClosed(2, maxPrime)
+                .filter(number -> primes.stream().allMatch(prime -> number % prime > 0))
+                .forEach(primes::add);
+        return primes;
     }
 }
