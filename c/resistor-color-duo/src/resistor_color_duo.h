178590444c4f70c3053e20d29752218c5b3d5028
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-09 11:09:51 +0300

    Added minimum code to compile the program

--- c/resistor-color-duo/src/resistor_color_duo.h
+++ c/resistor-color-duo/src/resistor_color_duo.h
@@ -1,4 +1,12 @@
 #ifndef RESISTOR_COLOR_DUO_H
 #define RESISTOR_COLOR_DUO_H
 
+typedef enum {
+    BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE
+} resistor_band_t;
+
+int color_code(resistor_band_t[]);
+
+const resistor_band_t *colors(void);
+
 #endif
