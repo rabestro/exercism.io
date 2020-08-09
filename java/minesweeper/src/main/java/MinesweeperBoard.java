da6ae1db5ead0f7579f855711a7b5a7eb685533c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-09 21:20:50 +0300

    Stream API

--- java/minesweeper/src/main/java/MinesweeperBoard.java
+++ java/minesweeper/src/main/java/MinesweeperBoard.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.lang.Math.max;
 import static java.lang.Math.min;
@@ -17,30 +17,18 @@ public class MinesweeperBoard {
     }
 
     public List<String> withNumbers() {
-        final var list = new ArrayList<String>();
-        for (int row = 0; row < rows; row++) {
-            var line = new char[cols];
-            for (int col = 0; col < cols; col++) {
-                if (inputBoard.get(row).charAt(col) == '*') {
-                    line[col] = '*';
-                } else {
-                    int mines = countMines(row, col);
-                    if (mines == 0) {
-                        line[col] = ' ';
-                    } else {
-                        line[col] = Character.forDigit(mines, 10);
-                    }
-                }
-            }
-            list.add(new String(line));
-        }
-        return list;
+        return range(0, rows).mapToObj(row -> range(0, cols)
+                .map(col -> inputBoard.get(row).charAt(col) == '*' ? '*' : countMines(row, col))
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString()
+        ).collect(Collectors.toList());
     }
 
-    private int countMines(final int row, final int col) {
-        return (int) range(max(0, row - 1), min(rows, row + 2))
+    private char countMines(final int row, final int col) {
+        final int mines = (int) range(max(0, row - 1), min(rows, row + 2))
                 .flatMap(i -> range(max(0, col - 1), min(cols, col + 2))
                         .filter(j -> inputBoard.get(i).charAt(j) == '*'))
                 .count();
+        return mines == 0 ? ' ' : Character.forDigit(mines, 10);
     }
 }
