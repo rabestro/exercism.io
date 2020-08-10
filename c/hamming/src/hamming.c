fd01ec1f65ab3acdd9b755bfd67895943a2d115d
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-10 12:22:00 +0300

    Added minimum code to pass the next test.

--- c/hamming/src/hamming.c
+++ c/hamming/src/hamming.c
@@ -1,6 +1,9 @@
 #include "hamming.h"
+#include <stddef.h>
 
 int compute(const char *lhs, const char *rhs) {
+    if (lhs == NULL || rhs == NULL) return -1;
+
     int difference = 0;
     while (*lhs != '\0' && *rhs != '\0') {
         if (*lhs++ != *rhs++) ++difference;
--- c/hamming/test/test_hamming.c
+++ c/hamming/test/test_hamming.c
@@ -16,7 +16,6 @@ static void test_empty_strands(void)
 
 static void test_rejects_null_strand(void)
 {
-   TEST_IGNORE();               // delete this line to run test
    TEST_ASSERT_EQUAL(-1, compute(NULL, "A"));
 }
 
