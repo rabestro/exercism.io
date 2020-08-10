ae080cbb934307406d1ff25c767b9481f5f9eb8d
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-10 12:19:53 +0300

    Added minimum code to pass the first test.

--- c/hamming/src/hamming.c
+++ c/hamming/src/hamming.c
@@ -1,1 +1,9 @@
 #include "hamming.h"
+
+int compute(const char *lhs, const char *rhs) {
+    int difference = 0;
+    while (*lhs != '\0' && *rhs != '\0') {
+        if (*lhs++ != *rhs++) ++difference;
+    }
+    return difference;
+}
