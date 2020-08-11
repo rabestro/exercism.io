49a15b9137d6767d8936f190f8bacb1f328f1dc9
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-11 16:25:39 +0300

    Added minimum code to pass the next code

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
 
-#isQuestion=[[ $1 =~ "\?$" ]]
 readonly sentence=${1//[!a-zA-Z?]/}
-
-#echo "$isQuestion"
-
-if [[ $sentence == "${sentence^^}" ]]; then
-  isUpper=true
-else
-  isUpper=false
-fi
 
 if [[ $1 =~ ^[[:space:]\\t\\n]*$ ]]; then
   echo "Fine. Be that way!"
 elif [[ ${#sentence} -gt 1 && $sentence == "${sentence^^}" && $sentence =~ \?$ ]]; then
   echo "Calm down, I know what I'm doing!"
-elif [[ ${#sentence} -gt 0 && $sentence == "${sentence^^}" ]]; then
-  echo "Whoa, chill out!"
 elif [[ $sentence =~ \?$ ]]; then
   echo "Sure."
+elif [[ ${#sentence} -gt 0 && $sentence == "${sentence^^}" ]]; then
+  echo "Whoa, chill out!"
 else
   echo "Whatever."
 fi
