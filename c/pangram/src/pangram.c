ad79b2136ea821aebd3258e7e818fed639d828e9
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-11 10:24:01 +0300

    Added minimum code to pass the very first test.

--- c/pangram/src/pangram.c
+++ c/pangram/src/pangram.c
@@ -1,6 +1,13 @@
 #include "pangram.h"
 #include <stddef.h>
+#include <ctype.h>
+#include <stdint.h>
 
 bool is_pangram(const char *sentence) {
-    return sentence != NULL;
+    if (sentence == NULL) return false;
+    uint32_t set = 0;
+    while (*sentence != '\0') {
+        if (isalpha(*sentence)) set |= 1 << (tolower(*sentence) - 'a');
+    }
+    return set == (1 << 27) - 1;
 }
