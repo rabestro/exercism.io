621e1c3ba72f97b7b503a34bb888e669734cd734
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-11 16:35:17 +0300

    All tests passed. Solved.

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-readonly sentence=${1//[!a-zA-Z?]/}
+readonly phrase=${1//[!a-zA-Z?]/}
 
 if [[ $1 =~ ^[[:space:]\\t\\n]*$ ]]; then
   echo "Fine. Be that way!"
-elif [[ ${#sentence} -gt 1 && $sentence == "${sentence^^}" && $sentence =~ \?$ ]]; then
+elif [[  $phrase =~ ^[A-Z]+\?$ ]]; then
   echo "Calm down, I know what I'm doing!"
-elif [[ $sentence =~ \?$ ]]; then
+elif [[ $phrase =~ \?$ ]]; then
   echo "Sure."
-elif [[ ${#sentence} -gt 0 && $sentence == "${sentence^^}" ]]; then
+elif [[ $phrase =~ ^[A-Z]+$ ]]; then
   echo "Whoa, chill out!"
 else
   echo "Whatever."
