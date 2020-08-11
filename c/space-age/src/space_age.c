f3ff48e56c34456f5157e1104dbdb544501296e3
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-11 09:59:47 +0300

    Added minimum code to pass the first test.

--- c/space-age/src/space_age.c
+++ c/space-age/src/space_age.c
@@ -1,1 +1,17 @@
 #include "space_age.h"
+
+float convert_planet_age(planet_t planet, int64_t input) {
+    switch (planet) {
+        case EARTH:
+            return input / 31557600.0;
+        case MERCURY:
+        case VENUS:
+        case MARS:
+        case JUPITER:
+        case SATURN:
+        case URANUS:
+        case NEPTUNE:
+        default:
+            return 0.0;
+    }
+}
