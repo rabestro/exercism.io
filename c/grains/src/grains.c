b1bfbaddce88f0bee244388f982f0c4c17330e84
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-11 09:14:19 +0300

    All tests passed. Solved.

--- c/grains/src/grains.c
+++ c/grains/src/grains.c
@@ -1,1 +1,9 @@
 #include "grains.h"
+
+uint64_t square(uint8_t index) {
+    return (0 < index && index <= 64) ? 1l << (index - 1) : 0;
+}
+
+uint64_t total(void) {
+    return 0xFFFFFFFFFFFFFFFF;
+}
