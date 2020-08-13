67a6d0a4feb5f2aca1ef0b51a45cb166513fe86d
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-13 21:28:16 +0300

    All tests passed. Solved.

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,18 +1,11 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Given a moment, determine the moment that would be after a gigasecond has passed.
+//A gigasecond is 10^9 (1,000,000,000) seconds.
 package gigasecond
 
 // import path for the time package from the standard library
 import "time"
 
-// AddGigasecond should have a comment documenting it.
+// AddGigasecond adds a gigasecond to the given time
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return t
+	return t.Add(1_000_000_000 * time.Second)
 }
