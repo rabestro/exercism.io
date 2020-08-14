73f1c592e03a55e3bdc57c38eff1c2d08c2c6f7c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-15 00:01:07 +0300

    All tests passed. Solved

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -6,5 +6,6 @@ func ToRomanNumeral(number int) (string, error) {
 	if number < 1 || number > 3000 {
 		return "", errors.New("number has to be in range [1..3000]")
 	}
-	return []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}[number%10], nil
+	roman := []string{"", "M", "MM", "MMM"}[number/1000] + []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}[number%1000/100] + []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}[number%100/10] + []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}[number%10]
+	return roman, nil
 }
