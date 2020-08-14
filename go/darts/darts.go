6bee6ddef555a06b4c6be1d1c7c13c650bcc391b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-14 23:12:32 +0300

    All tests passed. Solved.

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,1 +1,17 @@
 package darts
+
+import "math"
+
+func Score(x float64, y float64) int {
+	distance := math.Sqrt(x*x + y*y)
+	if distance > 10 {
+		return 0
+	}
+	if distance > 5 {
+		return 1
+	}
+	if distance > 1 {
+		return 5
+	}
+	return 10
+}
