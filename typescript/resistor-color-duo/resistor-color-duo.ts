d3488acd4ab6253ff7fd728cd22fac179c6814da
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-14 22:48:09 +0300

    Add problem "Darts"

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,17 @@
 export class ResistorColor {
-  private colors: string[];
 
-  constructor(colors: string[]) {
-    this.colors = colors;
-  }
-  value = (): number => 0;
+    private readonly colors: string[];
+
+    constructor(colors: string[]) {
+        this.colors = colors;
+    }
+
+    value = (): number => {
+        const colorsMap: Map<string, number> = new Map([
+            ['black', 0],
+            ['brown', 1]
+        ]);
+        // colorsMap.has('brown') ? colorsMap.get('brown') : 0
+        return colorsMap.get('brown') * 10;
+    }
 }
