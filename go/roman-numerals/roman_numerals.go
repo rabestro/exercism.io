3850526a3915a6c1b4d3f697cce02b95c52b7b9a
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-14 23:52:59 +0300

    Added minimum code to pass next tests

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -1,6 +1,10 @@
 package romannumerals
 
-func ToRomanNumeral(number int) (string, error) {
+import "errors"
 
-	return "I", nil
+func ToRomanNumeral(number int) (string, error) {
+	if number < 1 || number > 3000 {
+		return "", errors.New("number has to be in range [1..3000]")
+	}
+	return []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}[number%10], nil
 }
