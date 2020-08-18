8cccda8b47929244353d0edbaab38ebdfe278103
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-18 12:36:50 +0300

    All tests passed. Solved.

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,8 +1,9 @@
-//
-// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// Given a DNA strand, return its RNA complement (per RNA transcription).
 
-export const toRna = () => {
-  return ''
+export const toRna = (dna) => {
+    const transcription = {G: 'C', C: 'G', T: 'A', A: 'U'};
+    return dna
+        .split('')
+        .map(nucleotide => transcription[nucleotide])
+        .join('');
 };
