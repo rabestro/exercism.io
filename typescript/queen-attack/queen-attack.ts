93cd6d599e87db697de6d7b69dacd924e9704157
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-18 16:01:11 +0300

    Added class QueenAttack

---
+++ typescript/queen-attack/queen-attack.ts
@@ -0,0 +1,16 @@
+class QueenAttack {
+
+    public white: any;
+    public black: any;
+
+    constructor(positioning: any) {
+        console.log(positioning);
+
+    }
+
+    canAttack() {
+        return undefined;
+    }
+}
+
+export default QueenAttack
