3a51af7e71425a2b750225dc80be07456f909415
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-04 19:00:08 +0300

    Solved

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 readonly cell=$1
+if [[ $cell == "total" ]]; then
+  echo 18446744073709551615
+  exit 0
+fi
 if ((cell < 1 || cell > 64)); then
   echo "Error: invalid input"
   exit 1
 fi
-grains=$((1 << cell - 1))
+if ((cell == 64)); then
+  echo 9223372036854775808
+  exit 0
+fi
+readonly grains=$((1 << cell - 1))
 echo $grains
