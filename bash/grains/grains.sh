2b5c2d2f7ee648d8a07059f9fb03eb4104717a70
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-04 18:53:39 +0300

    Added minimum code to pass the next tests

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 readonly cell=$1
-grains=$((1<<cell-1))
+if ((cell < 1 || cell > 64)); then
+  echo "Error: invalid input"
+  exit 1
+fi
+grains=$((1 << cell - 1))
 echo $grains
