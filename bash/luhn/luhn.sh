9004a08476b6c42d7b2ffc01c21de2a13c463790
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-06 19:48:39 +0300

    Solved

--- bash/luhn/luhn.sh
+++ bash/luhn/luhn.sh
@@ -1,9 +1,31 @@
 #!/usr/bin/env bash
 
-readonly number=${1//[[:blank:]]/}
+declare -r number=${1//[[:blank:]]/}
 
-if ! [[ $$number =~ ^[[:digit:]]{2,}$ ]]; then
+if ! [[ $number =~ ^[[:digit:]]{2,}$ ]]; then
   echo false
   exit 0
 fi
 
+declare -i sum=0
+declare -i len=${#number}
+declare -i isEven=$((len % 2))
+declare -i digit
+
+for ((i = 0; i < len; i++)); do
+  digit=${number:i:1}
+  if ((isEven == 0)); then
+    ((digit *= 2))
+    if ((digit > 9)); then
+      ((digit -= 9))
+    fi
+  fi
+  ((sum+=digit))
+  ((isEven=1-isEven))
+done
+
+if ((sum % 10 == 0)); then
+  echo true
+else
+  echo false
+fi
