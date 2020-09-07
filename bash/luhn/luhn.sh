e3cb875ddcf7caba8251d9b51577a708eaaa8750
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-07 10:38:06 +0300

    Add project "Atbash Cipher"

--- bash/luhn/luhn.sh
+++ bash/luhn/luhn.sh
@@ -9,19 +9,19 @@ fi
 
 declare -i sum=0
 declare -i len=${#number}
-declare -i isEven=$((len % 2))
+declare -i isOdd=$((len % 2))
 declare -i digit
 
 for ((i = 0; i < len; i++)); do
   digit=${number:i:1}
-  if ((isEven == 0)); then
+  if ((!isOdd)); then
     ((digit *= 2))
     if ((digit > 9)); then
       ((digit -= 9))
     fi
   fi
   ((sum+=digit))
-  ((isEven=1-isEven))
+  ((isOdd=!isOdd))
 done
 
 if ((sum % 10 == 0)); then
