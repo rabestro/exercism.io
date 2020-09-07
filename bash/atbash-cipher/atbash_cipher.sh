b60dc5a890a4cba5b857095fd09e1c63a6aceb20
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-07 11:29:15 +0300

    Added variable symbol

--- bash/atbash-cipher/atbash_cipher.sh
+++ bash/atbash-cipher/atbash_cipher.sh
@@ -4,12 +4,13 @@ declare -A code_table=(
   [a]=z [b]=y [c]=x [d]=w [e]=v [f]=u [g]=t [h]=s [i]=r [j]=q [k]=p [l]=o [m]=n
   [n]=m [o]=l [p]=k [q]=j [r]=i [s]=h [t]=g [u]=f [v]=e [w]=d [x]=c [y]=b [z]=a)
 
-declare -r input=${1//[[:blank:][:punct:]]/}
+declare -r input=${2//[[:blank:][:punct:]]/}
 declare -r phrase=${input,,}
 declare -i len=${#phrase}
 
 for ((i = 0; i < len; i++)); do
-  result=$result${code_table[${phrase:i:1}]}
+  symbol=${code_table[${phrase:i:1}]}
+  result=$result$symbol
 done
 
-printf "%s" "$result"
+echo "$result"
