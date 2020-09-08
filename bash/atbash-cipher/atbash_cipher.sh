97a46f529925f4fb526780b19c400c14e9e81f1b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-09-08 19:19:34 +0300

    Refined code according to the mentors recommendations

--- bash/atbash-cipher/atbash_cipher.sh
+++ bash/atbash-cipher/atbash_cipher.sh
@@ -4,8 +4,7 @@ declare -A code_table=(
   [a]=z [b]=y [c]=x [d]=w [e]=v [f]=u [g]=t [h]=s [i]=r [j]=q [k]=p [l]=o [m]=n
   [n]=m [o]=l [p]=k [q]=j [r]=i [s]=h [t]=g [u]=f [v]=e [w]=d [x]=c [y]=b [z]=a)
 
-declare -r input=${2//[[:blank:][:punct:]]/}
-declare -r phrase=${input,,}
+declare -rl phrase=${2//[^[:alnum:]]/}
 declare -i len=${#phrase}
 
 for ((i = 0; i < len; i++)); do
@@ -13,11 +12,11 @@ for ((i = 0; i < len; i++)); do
   if [[ $symbol =~ [[:alpha:]] ]]; then
     symbol=${code_table[$symbol]}
   fi
-  result=$result$symbol
+  result+=$symbol
 done
 
 if [[ $1 == encode ]]; then
-  echo "$result" | sed 's/.\{5\}/& /g' | sed 's/ *$//g'
+  echo "$result" | sed -e 's/.\{5\}/& /g' -e 's/ *$//'
 else
   echo "$result"
 fi
