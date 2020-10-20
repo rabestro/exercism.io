4520a768c73cfbdf61bda242a45e3916b9cfa8f8
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-10-20 11:47:37 +0300

    Code optimized according to the mentor recommendations: "Simple code, but inefficient since it would create lot of intermediate lists and abandon them. What if you could write it as Stream flatten(Stream input) and finally collect them?"

--- java/flatten-array/src/main/java/Flattener.java
+++ java/flatten-array/src/main/java/Flattener.java
@@ -6,9 +6,11 @@ import java.util.stream.Stream;
 class Flattener {
 
     List flatten(List list) {
-        return (List) list.stream()
-                .flatMap(o -> o instanceof List ? flatten((List) o).stream() : Stream.of(o))
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
+        return (List) flatten(list.stream()).collect(Collectors.toList());
+    }
+
+    Stream flatten(Stream stream) {
+        return stream.flatMap(o -> o instanceof List ? flatten(((List) o).stream()) : Stream.of(o))
+                .filter(Objects::nonNull);
     }
 }
