45de261611d0135c795c54dac2953d95cab4f096
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-11-26 22:01:22 +0200

    Added minimum code to pass the first test

--- java/linked-list/src/main/java/DoublyLinkedList.java
+++ java/linked-list/src/main/java/DoublyLinkedList.java
@@ -1,10 +1,32 @@
-public class DoublyLinkedList<T> {
+import static java.util.Objects.isNull;
 
-    public void push(T i) {
+public class DoublyLinkedList<T> {
+    private Box<T> head;
+    private Box<T> tail;
+
+    public void push(T data) {
+        final var box = new Box<>(data);
+
+        if (isNull(head)) {
+            head = box;
+            tail = head;
+            return;
+        }
+        tail.setNext(box);
+        box.setPrev(tail);
+        tail = box;
     }
 
     public T pop() {
-        return null;
+        if (tail == null) {
+            return null;
+        }
+        final var box = tail;
+        tail = tail.getPrev();
+        if (isNull(tail)) {
+            head  = null;
+        }
+        return box.getData();
     }
 
     public T shift() {
@@ -13,5 +35,31 @@ public class DoublyLinkedList<T> {
 
     public void unshift(T c) {
 
+    }
+
+    private static class Box<T> {
+        private Box<T> prev;
+        private Box<T> next;
+        private T data;
+
+        public Box(T data) {
+            this.data = data;
+        }
+
+        public void setNext(Box<T> element) {
+            next = element;
+        }
+
+        public void setPrev(Box<T> element) {
+            prev = element;
+        }
+
+        public Box<T> getPrev() {
+            return prev;
+        }
+
+        public T getData() {
+            return data;
+        }
     }
 }
