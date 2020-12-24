b80fbe64f88d359c32b340491cf6c1dbe1cf5aa3
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-12-24 23:19:40 +0200

    Added minimum code to pass the first test

--- go/hello-world/hello_world.go
+++
@@ -1,8 +0,0 @@
-// Package greeting should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
-package greeting
-
-// HelloWorld should have a comment documenting it.
-func HelloWorld() string {
-	return "Hello, World!"
-}
--- go/leap/leap.go
+++
@@ -1,7 +0,0 @@
-// Package leap contains a function that says if a year is leap or not
-package leap
-
-// IsLeapYear returns true if given a year is a leap year, otherwise false.
-func IsLeapYear(year int) bool {
-	return year%400 == 0 || (year%4 == 0 && year%100 > 0)
-}
