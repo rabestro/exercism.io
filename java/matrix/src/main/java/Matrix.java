f1973b4b424112d356e037de0a2a2bdc142e44a9
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-06 19:33:34 +0200

    Added final modifier

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -5,21 +5,22 @@ import static java.util.stream.IntStream.range;
 
 class Matrix {
     private static final Pattern SPACE = Pattern.compile("\\s");
+
     private final int[] matrix;
     private final int rows;
     private final int cols;
 
-    Matrix(String matrixAsString) {
+    Matrix(final String matrixAsString) {
         rows = (int) matrixAsString.lines().count();
         matrix = SPACE.splitAsStream(matrixAsString).mapToInt(Integer::parseInt).toArray();
         cols = matrix.length / rows;
     }
 
-    int[] getRow(int rowNumber) {
+    int[] getRow(final int rowNumber) {
         return copyOfRange(matrix, cols * (rowNumber - 1), cols * rowNumber);
     }
 
-    int[] getColumn(int columnNumber) {
+    int[] getColumn(final int columnNumber) {
         return range(0, rows).map(i -> matrix[i * cols + columnNumber - 1]).toArray();
     }
 
