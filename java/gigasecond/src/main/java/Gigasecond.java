ce918655be795d9a68eb27cd1d0f1288b0007f76
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-06 19:14:35 +0200

    Added constructor chain to avoid code duplication

--- java/gigasecond/src/main/java/Gigasecond.java
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -4,10 +4,11 @@ import java.time.LocalTime;
 
 public class Gigasecond {
     private static final long GIGASECOND = 1_000_000_000;
+
     private final LocalDateTime moment;
 
     public Gigasecond(LocalDate moment) {
-        this.moment = LocalDateTime.of(moment, LocalTime.of(0,0)).plusSeconds(GIGASECOND);
+        this(moment.atStartOfDay());
     }
 
     public Gigasecond(LocalDateTime moment) {
