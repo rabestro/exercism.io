f3ee2d1e28a4823ff5de177c5446d1ccfad0cba5
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-08 17:22:41 +0200

    Regular expressions simplified and compiled.

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,22 +1,22 @@
-import java.util.StringJoiner;
 import java.util.regex.Pattern;
+
+import static java.util.stream.Collectors.joining;
 
 public final class PigLatinTranslator {
     private static final Pattern WORDS_DELIMITER = Pattern.compile("\\s+");
+    private static final Pattern BEGIN_WITH_VOWEL = Pattern.compile("^([aeoui]|xr|yt)");
+    private static final Pattern RULES234 = Pattern.compile("^([^aeoui]?qu|[^aeoui][^aeouiy]*)(.*)");
+    private static final String RULES234_REPLACE = "$2$1ay";
 
     public String translate(String sentence) {
-        final var result = new StringJoiner(" ");
-
-        WORDS_DELIMITER
+        return WORDS_DELIMITER
                 .splitAsStream(sentence)
                 .map(this::toPigLatin)
-                .forEach(result::add);
-
-        return result.toString();
+                .collect(joining(" "));
     }
     private String toPigLatin(String word) {
-        return word.matches("([aeoui]|xr|yt).+")
+        return BEGIN_WITH_VOWEL.matcher(word).find()
                 ? word + "ay"
-                : word.replaceFirst("(qu|[^aeoui][^aeouiy]*?qu|[^aeoui][^aeouiy]*)(.*)", "$2$1ay");
+                : RULES234.matcher(word).replaceFirst(RULES234_REPLACE);
     }
 }
