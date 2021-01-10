48c1f0f1077a7942c63725b4541d531f57bffc7b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-10 11:12:49 +0200

    Added named groups

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -5,8 +5,8 @@ import static java.util.stream.Collectors.joining;
 public final class PigLatinTranslator {
     private static final Pattern WORDS_DELIMITER = Pattern.compile("\\s+");
     private static final Pattern PIG_LATIN_RULES = Pattern.compile(
-            "^(([aeoui]|xr|yt)|([^aeoui]?qu|[^aeoui][^aeouiy]*)?)(.*)");
-    private static final String PIG_LATIN_REPLACE = "$2$4$3ay";
+            "^((?<vowel>[aeoui]|xr|yt)|(?<consonant>[^aeoui]?qu|[^aeoui][^aeouiy]*)?)(?<body>.*)");
+    private static final String PIG_LATIN_REPLACE = "${vowel}${body}${consonant}ay";
 
     public String translate(String sentence) {
         return WORDS_DELIMITER
