7aa88975fac183c851e381575e58b1038d286391
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-10 12:00:41 +0200

    Regexp optimized

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -5,7 +5,7 @@ import static java.util.stream.Collectors.joining;
 public final class PigLatinTranslator {
     private static final Pattern WORDS_DELIMITER = Pattern.compile("\\s+");
     private static final Pattern PIG_LATIN_RULES = Pattern.compile(
-            "^((?<vowel>[aeoui]|xr|yt)|(?<consonant>[^aeoui]?qu|[^aeoui][^aeouiy]*))(?<body>.*)");
+            "((?<vowel>[aeoui]|xr|yt)|(?<consonant>[^aeoui]?qu|[^aeoui][^aeouiy]*))(?<body>.*)");
     private static final String PIG_LATIN_REPLACE = "${vowel}${body}${consonant}ay";
 
     public String translate(String sentence) {
@@ -15,6 +15,8 @@ public final class PigLatinTranslator {
                 .collect(joining(" "));
     }
     private String toPigLatin(String word) {
-        return PIG_LATIN_RULES.matcher(word).replaceFirst(PIG_LATIN_REPLACE);
+        return PIG_LATIN_RULES
+                .matcher(word)
+                .replaceFirst(PIG_LATIN_REPLACE);
     }
 }
