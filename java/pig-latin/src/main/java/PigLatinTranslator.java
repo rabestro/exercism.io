17b06207bc5f5a84c9b1f7950fc58b3d2b87789d
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-10 11:15:07 +0200

    Regexp optimized

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -5,7 +5,7 @@ import static java.util.stream.Collectors.joining;
 public final class PigLatinTranslator {
     private static final Pattern WORDS_DELIMITER = Pattern.compile("\\s+");
     private static final Pattern PIG_LATIN_RULES = Pattern.compile(
-            "^((?<vowel>[aeoui]|xr|yt)|(?<consonant>[^aeoui]?qu|[^aeoui][^aeouiy]*)?)(?<body>.*)");
+            "^((?<vowel>[aeoui]|xr|yt)|(?<consonant>[^aeoui]?qu|[^aeoui][^aeouiy]*))(?<body>.*)");
     private static final String PIG_LATIN_REPLACE = "${vowel}${body}${consonant}ay";
 
     public String translate(String sentence) {
