bdeced9fc4b223c701acf6905cc5ec9eb312a818
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-11 20:19:41 +0200

    Code refined

--- java/nth-prime/src/main/java/PrimeCalculator.java
+++ java/nth-prime/src/main/java/PrimeCalculator.java
@@ -2,27 +2,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 class PrimeCalculator {
-    List<Integer> primeNumbers = new ArrayList<>(List.of(2, 3, 5, 7, 11, 13));
+    List<Integer> primes = new ArrayList<>(List.of(2, 3, 5, 7, 11, 13));
 
     int nth(int nth) {
         if (nth < 1) {
             throw new IllegalArgumentException();
         }
-        if (nth > primeNumbers.size()) {
+        if (nth > primes.size()) {
             calculatePrime(nth);
         }
-        return primeNumbers.get(nth - 1);
+        return primes.get(nth - 1);
     }
 
     void calculatePrime(int nth) {
-        for (int i = primeNumbers.get(primeNumbers.size() - 1) + 1; nth > primeNumbers.size(); ++i) {
+        final int maxPrime = primes.get(primes.size() - 1);
+
+        for (int i = maxPrime + 1; nth > primes.size(); ++i) {
             if (isPrime(i)) {
-                primeNumbers.add(i);
+                primes.add(i);
             }
         }
     }
 
     boolean isPrime(int number) {
-        return primeNumbers.stream().allMatch(prime -> number % prime > 0);
+        return primes.stream().allMatch(prime -> number % prime > 0);
     }
 }
