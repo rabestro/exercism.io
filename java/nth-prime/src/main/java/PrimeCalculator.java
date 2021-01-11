1cbbd902d667ba396f2210b4e8c5af940921529e
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-11 19:56:41 +0200

    Added minimum code to pass the very first test

--- java/nth-prime/src/main/java/PrimeCalculator.java
+++ java/nth-prime/src/main/java/PrimeCalculator.java
@@ -1,7 +1,7 @@
 class PrimeCalculator {
 
     int nth(int nth) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return 2;
     }
 
 }
