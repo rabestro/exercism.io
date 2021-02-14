09f98173c2d4b3b598d98a0ff39b651be6998336
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-14 15:13:32 +0200

    Added minimum code to pass the very first test.

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,5 +1,7 @@
 #include "reverse_string.h"
 
 namespace reverse_string {
-
+    std::string reverse_string(std::string phrase) {
+        return phrase;
+    }
 }  // namespace reverse_string
--- cpp/reverse-string/reverse_string.h
+++ cpp/reverse-string/reverse_string.h
@@ -1,8 +1,12 @@
 #if !defined(REVERSE_STRING_H)
 #define REVERSE_STRING_H
 
-namespace reverse_string {
+#include <string>
 
+using std::string;
+
+namespace reverse_string {
+    std::string reverse_string(std::string phrase);
 }  // namespace reverse_string
 
 #endif // REVERSE_STRING_H
