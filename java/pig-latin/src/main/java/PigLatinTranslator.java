b047418029270ee5999d836af65c56bb2df7137c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-15 12:15:00 +0200

    Update PigLatinTranslator.java

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -4,9 +4,9 @@ import static java.util.stream.Collectors.joining;
 
 public final class PigLatinTranslator {
     private static final Pattern WORDS_DELIMITER = Pattern.compile("\\s+");
-    private static final Pattern PIG_LATIN_RULES = Pattern.compile(
+    private static final Pattern RULES = Pattern.compile(
             "((?<vowel>[aeoui]|xr|yt)|(?<consonant>[^aeoui]?qu|[^aeoui][^aeouiy]*))(?<body>.*)");
-    private static final String PIG_LATIN_REPLACE = "${vowel}${body}${consonant}ay";
+    private static final String TEMPLATE = "${vowel}${body}${consonant}ay";
 
     public String translate(String sentence) {
         return WORDS_DELIMITER
@@ -15,8 +15,6 @@ public final class PigLatinTranslator {
                 .collect(joining(" "));
     }
     private String toPigLatin(String word) {
-        return PIG_LATIN_RULES
-                .matcher(word)
-                .replaceFirst(PIG_LATIN_REPLACE);
+        return RULES.matcher(word).replaceFirst(TEMPLATE);
     }
 }
