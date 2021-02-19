023d103eb7c92698b5fd6247a644154f127cabc3
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-19 23:26:50 +0200

    Code optimization

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,11 +1,13 @@
 public class PangramChecker {
-    private final static int LETTERS_IN_ALPHABET = 26;
+    private final static int ALPHABET = 'Z' - 'A' + 1;
 
     public boolean isPangram(String input) {
-        return input.toLowerCase().chars()
+        return input.chars()
                 .filter(Character::isLetter)
+                .map(Character::toLowerCase)
                 .distinct()
-                .count() == LETTERS_IN_ALPHABET;
+                .limit(ALPHABET)
+                .count() == ALPHABET;
     }
 
 }
