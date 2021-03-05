83d01a5a2b93686fcc53adb148f7065c9e78f15b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-05 22:38:18 +0200

    Solved

--- groovy/hamming/src/main/groovy/Hamming.groovy
+++ groovy/hamming/src/main/groovy/Hamming.groovy
@@ -1,10 +1,14 @@
 class Hamming {
 
-    def distance(strand1, strand2) {
-        if (strand1.length() != strand2.length()) {
-            throw new ArithmeticException()
-        }
-        0
+    int distance(String strand1, String strand2) {
+        if (strand1.length() != strand2.length()) throw new ArithmeticException()
+        if (strand1.isEmpty()) return 0;
+
+        int difference = 0
+        int i = strand2.length()
+        while (i-- > 0) if (strand2[i] != strand1[i]) difference++
+
+        difference
     }
 
 }
