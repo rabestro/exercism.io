bd8b4a013d5908d7a346ae42d0fd8636adc242e0
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-05 20:07:25 +0200

    Added some code

--- groovy/anagram/src/main/groovy/Anagram.groovy
+++ groovy/anagram/src/main/groovy/Anagram.groovy
@@ -1,6 +1,22 @@
-class Anagram {
+import static java.util.function.Predicate.not;
+import static java.util.stream.Collectors.toUnmodifiableList;
 
-    def find(candidates) {
-        throw new UnsupportedOperationException('method not implemented.')
+class Anagram {
+    String subject
+
+    def find(List<String> candidates) {
+        sortedChars = toSortedChars(subject)
+
+        return candidates.stream()
+                .filter{ word -> !subject.equalsIgnoreCase(word) && isAnagram(sortedChars, word)}
+                .toList()
+    }
+
+    private static boolean isAnagram(int[] sortedChars, String otherWord) {
+        Arrays.equals(sortedChars, toSortedChars(otherWord))
+    }
+
+    private static int[] toSortedChars(String word) {
+        word.toLowerCase().chars().sorted().toArray()
     }
 }
