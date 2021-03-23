b43777f9081b2e0526abe4df8ae270c0e8868aa5
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-23 17:37:15 +0200

    Code optimization: added static final constants

--- java/resistor-color/src/main/java/ResistorColor.java
+++ java/resistor-color/src/main/java/ResistorColor.java
@@ -1,14 +1,18 @@
 import java.util.List;
 
 class ResistorColor {
-    private static final List<String> colors = List.of(
-            "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white");
+
+    private static final String[] COLORS = new String[]{
+            "black", "brown", "red", "orange", "yellow",
+            "green", "blue", "violet", "grey", "white"};
+
+    private static final List<String> COLORS_LIST = List.of(COLORS);
 
     int colorCode(String color) {
-        return colors.indexOf(color);
+        return COLORS_LIST.indexOf(color);
     }
 
     String[] colors() {
-        return colors.toArray(String[]::new);
+        return COLORS;
     }
 }
