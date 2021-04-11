a850f8741002d28d623fc971257960edcbeb1228
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-04-11 19:53:25 +0300

    Constant renamed

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -4,20 +4,18 @@ import static java.util.stream.Collectors.joining;
 
 public final class PigLatinTranslator {
     private static final Pattern WORDS_DELIMITER = Pattern.compile("\\s+");
-    private static final Pattern PIG_LATIN_RULES = Pattern.compile(
+    private static final Pattern RULES = Pattern.compile(
             "((?!yt|xr)(?<consonant>[^aeoui]?qu|[^aeoui][^aeouiy]*))?(?<body>.*)");
-    private static final String PIG_LATIN_REPLACE = "${body}${consonant}ay";
+    private static final String TEMPLATE = "${body}${consonant}ay";
 
     public String translate(String sentence) {
         return WORDS_DELIMITER
                 .splitAsStream(sentence)
-                .map(this::toPigLatin)
+                .map(this::translateWord)
                 .collect(joining(" "));
     }
 
-    private String toPigLatin(String word) {
-        return PIG_LATIN_RULES
-                .matcher(word)
-                .replaceFirst(PIG_LATIN_REPLACE);
+    private String translateWord(String word) {
+        return RULES.matcher(word).replaceFirst(TEMPLATE);
     }
 }
