3d453f16cb841938ffc7362c171608a962d28cad
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-04-11 19:47:40 +0300

    Optimized regex

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -4,17 +4,20 @@ import static java.util.stream.Collectors.joining;
 
 public final class PigLatinTranslator {
     private static final Pattern WORDS_DELIMITER = Pattern.compile("\\s+");
-    private static final Pattern RULES = Pattern.compile(
-            "((?<vowel>[aeoui]|xr|yt)|(?<consonant>[^aeoui]?qu|[^aeoui][^aeouiy]*))(?<body>.*)");
-    private static final String TEMPLATE = "${vowel}${body}${consonant}ay";
+    private static final Pattern PIG_LATIN_RULES = Pattern.compile(
+            "((?!yt|xr)(?<consonant>[^aeoui]?qu|[^aeoui][^aeouiy]*))?(?<body>.*)");
+    private static final String PIG_LATIN_REPLACE = "${body}${consonant}ay";
 
     public String translate(String sentence) {
         return WORDS_DELIMITER
                 .splitAsStream(sentence)
-                .map(this::translateWord)
+                .map(this::toPigLatin)
                 .collect(joining(" "));
     }
-    private String translateWord(String word) {
-        return RULES.matcher(word).replaceFirst(TEMPLATE);
+
+    private String toPigLatin(String word) {
+        return PIG_LATIN_RULES
+                .matcher(word)
+                .replaceFirst(PIG_LATIN_REPLACE);
     }
 }
