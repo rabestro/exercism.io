2c4f3003c94b10ca1820124d368077152b39fe5a
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-05-03 01:28:00 +0300

    Solved

--- java/say/src/main/java/Say.java
+++ java/say/src/main/java/Say.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.StringJoiner;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
@@ -17,19 +18,30 @@ public class Say {
         if (number == 0) {
             return numbers[0];
         }
-        final var units = List.of(" billion", " million", " thousand", " hundred", "").iterator();
+        final var units = List.of("billion", "million", "thousand", "").iterator();
 
         return LongStream.of(number / 1_000_000_000, number % 1_000_000_000 / 1_000_000,
-                number % 1_000_000 / 1_000, number % 1_000 / 100, number % 100)
+                number % 1_000_000 / 1_000, number % 1_000)
                 .mapToObj(i -> say((int) i, units.next()))
                 .filter(Predicate.not(String::isBlank))
                 .collect(Collectors.joining(" "));
-
     }
 
     private String say(int number, String unit) {
-        return number > 20 ? numbers[18 + number / 10] + "-" + numbers[number % 10] + unit :
-                number > 0 ? numbers[number] + unit : "";
+        if (number == 0) {
+            return "";
+        }
+        var sj = new StringJoiner(" ");
+        if (number >= 100) {
+            sj.add(say(number / 100, "hundred"));
+        }
+        number %= 100;
+        if (number > 20) {
+            sj.add(numbers[18 + number / 10] + "-" + numbers[number % 10]);
+        } else if (number > 0) {
+            sj.add(numbers[number]);
+        }
+        return unit.isBlank() ? sj.toString() : sj.add(unit).toString();
     }
 
 }
