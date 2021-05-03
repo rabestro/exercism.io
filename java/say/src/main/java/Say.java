1cc9232ad5c51d03bf750add24849d8ca3625140
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-05-03 20:48:04 +0300

    Update method say

--- java/say/src/main/java/Say.java
+++ java/say/src/main/java/Say.java
@@ -1,7 +1,8 @@
-import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.LongStream;
+
+import static java.util.Comparator.reverseOrder;
 
 public class Say {
     private static final String[] numbers = new String[]{
@@ -9,6 +10,8 @@ public class Say {
             "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
             "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
     };
+    private static final Map<Long, String> units = Map.of(
+            1_000_000_000L, " billion", 1_000_000L, " million", 1_000L, " thousand", 1L, "");
 
     public String say(long number) {
         if (number < 0 || number > 999_999_999_999L) {
@@ -17,15 +20,14 @@ public class Say {
         if (number == 0) {
             return numbers[0];
         }
-        final var units = List.of(" billion", " million", " thousand", "").iterator();
-        return LongStream.of(1_000_000_000, 1_000_000, 1_000, 1)
-                .mapToInt(i -> (int) (number % (i * 1000) / i))
-                .mapToObj(i -> say(i, units.next()))
+        return units.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey(reverseOrder()))
+                .map(e -> translate((int) (number % (e.getKey() * 1000) / e.getKey()), e.getValue()))
                 .filter(Predicate.not(String::isBlank))
                 .collect(Collectors.joining(" "));
     }
 
-    private String say(int number, String unit) {
+    private String translate(int number, String unit) {
         if (number < 1) {
             return "";
         }
@@ -36,8 +38,8 @@ public class Say {
             final var units = number % 10;
             return numbers[18 + number / 10] + (units == 0 ? "" : "-" + numbers[units]) + unit;
         }
-        final var units = say(number % 100);
-        return say(number / 100, " hundred") + (units.isBlank() ? "" : " " + units) + unit;
+        final var units = translate(number % 100, "");
+        return translate(number / 100, " hundred") + (units.isBlank() ? "" : " " + units) + unit;
     }
 
 }
