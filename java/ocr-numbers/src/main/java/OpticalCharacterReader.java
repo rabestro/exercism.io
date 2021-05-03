d2f17702be717beff7e37e2525bed43150eeff2e
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-05-03 20:54:51 +0300

    Add code to pass the very first test

--- java/ocr-numbers/src/main/java/OpticalCharacterReader.java
+++ java/ocr-numbers/src/main/java/OpticalCharacterReader.java
@@ -1,10 +1,8 @@
-/*
+import java.util.List;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class OpticalCharacterReader {
 
-Please remove this comment when submitting your solution.
-
-*/
+    public String parse(List<String> asList) {
+        return "0";
+    }
+}
