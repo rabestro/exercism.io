bae9ee26eb991439edd28cff120c1c377d9e26c5
Jegors Cemisovs <jegors.cemisovs@sap.com>
2021-05-12 22:01:46 +0300

    CU-jfacr5: Add minimum code to pass the very first test

--- java/complex-numbers/src/main/java/ComplexNumber.java
+++ java/complex-numbers/src/main/java/ComplexNumber.java
@@ -1,10 +1,45 @@
-/*
+public class ComplexNumber {
+    private double real;
+    private double imag;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    ComplexNumber(double r, double i) {
+        this.real = r;
+        this.imag = i;
+    }
 
-Please remove this comment when submitting your solution.
+    public double getReal() {
+        return real;
+    }
 
-*/
+    public double getImag() {
+        return imag;
+    }
+
+    public ComplexNumber times(ComplexNumber complexNumber) {
+        return null;
+    }
+
+    public ComplexNumber add(ComplexNumber complexNumber) {
+        return null;
+    }
+
+    public ComplexNumber minus(ComplexNumber complexNumber) {
+        return null;
+    }
+
+    public ComplexNumber div(ComplexNumber complexNumber) {
+        return null;
+    }
+
+    public double abs() {
+        return 0;
+    }
+
+    public ComplexNumber conjugate() {
+        return null;
+    }
+
+    public ComplexNumber exponentialOf() {
+        return null;
+    }
+}
