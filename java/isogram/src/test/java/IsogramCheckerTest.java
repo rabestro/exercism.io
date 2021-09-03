5b7d0bbea96cc7cff2b2c20afa3864a81cf0226e
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-09-03 21:53:02 +0300

    Updated solution

--- java/isogram/src/test/java/IsogramCheckerTest.java
+++ java/isogram/src/test/java/IsogramCheckerTest.java
@@ -1,7 +1,7 @@
-import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class IsogramCheckerTest {
 
