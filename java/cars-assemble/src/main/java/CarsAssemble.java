7aa2e99e5c8e0f4890723bd160f041220725b457
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-09-10 22:33:32 +0300

    Solved

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,18 +1,18 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        return successRate(speed) * 221 * speed / 100.0;
-
+        return cars(speed) / 100.0;
     }
 
     public int workingItemsPerMinute(int speed) {
-        return successRate(speed) * 221 * speed / 6000;
+        return cars(speed) / 6000;
     }
 
-    private int successRate(int speed) {
-        return speed < 5 ? 100
+    private int cars(int speed) {
+        int successRate = speed < 5 ? 100
                 : speed < 9 ? 90
                 : speed < 10 ? 80
                 : 77;
+        return successRate * 221 * speed;
     }
 }
