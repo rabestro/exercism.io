60af5be939c8e5df59fbcb68ffcf644ea5185136
Jegors Cemisovs <jegors.cemisovs@outlook.lv>
2021-09-15 22:00:49 +0300

    Solved LogLevels

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,34 @@
+import java.util.regex.Pattern;
+
 public class LogLevels {
-    
+
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+        return new Log(logLine).message;
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+        return new Log(logLine).level;
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+        final var log = new Log(logLine);
+        return log.message + " (" + log.level + ")";
+    }
+}
+
+class Log {
+    private static final Pattern LOG_PATTERN =
+            Pattern.compile("\\[(?<level>.+)]:\\s+(?<message>.+?)\\s*");
+    String message;
+    String level;
+
+    Log(String logLine) {
+        var matcher = LOG_PATTERN.matcher(logLine);
+        if (matcher.matches()) {
+            message = matcher.group("message");
+            level = matcher.group("level").toLowerCase();
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
