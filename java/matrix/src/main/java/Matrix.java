dfe38f15e1e446122be991a523c46dd666f79247
Jegors Cemisovs <jegors.cemisovs@outlook.lv>
2021-09-15 20:40:45 +0300

    Update Matrix

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -5,22 +5,21 @@ import static java.util.stream.IntStream.range;
 
 class Matrix {
     private static final Pattern SPACE = Pattern.compile("\\s");
-
     private final int[] matrix;
     private final int rows;
     private final int cols;
 
-    Matrix(final String matrixAsString) {
+    Matrix(String matrixAsString) {
         rows = (int) matrixAsString.lines().count();
         matrix = SPACE.splitAsStream(matrixAsString).mapToInt(Integer::parseInt).toArray();
         cols = matrix.length / rows;
     }
 
-    int[] getRow(final int rowNumber) {
+    int[] getRow(int rowNumber) {
         return copyOfRange(matrix, cols * (rowNumber - 1), cols * rowNumber);
     }
 
-    int[] getColumn(final int columnNumber) {
+    int[] getColumn(int columnNumber) {
         return range(0, rows).map(i -> matrix[i * cols + columnNumber - 1]).toArray();
     }
 
