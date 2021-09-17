462deb5ff781edb8ff813e8d04d5600adb9edc6a
Jegors Cemisovs <jegors.cemisovs@outlook.lv>
2021-09-17 17:30:15 +0300

    Update solution for LogLevels

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,20 +1,15 @@
 public class LogLevels {
 
     public static String message(String logLine) {
-        return parse(logLine)[1];
+        return logLine.substring(logLine.indexOf(":") + 1).trim();
     }
 
     public static String logLevel(String logLine) {
-        return parse(logLine)[0];
+        return logLine.substring(1, logLine.indexOf("]")).toLowerCase();
     }
 
     public static String reformat(String logLine) {
-        String[] log = parse(logLine);
-        return log[1] + " (" + log[0] + ")";
+        return message(logLine) + " (" + logLevel(logLine) + ")";
     }
 
-    private static String[] parse(String log) {
-        int index = log.indexOf(']');
-        return new String[]{log.substring(1, index).toLowerCase(), log.substring(index + 2).strip()};
-    }
 }
