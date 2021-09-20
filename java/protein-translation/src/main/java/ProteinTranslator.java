874d29361c2a1197bbdbb148474ef5b18a8f2e4b
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-09-20 20:29:06 +0300

    Solved

--- java/protein-translation/src/main/java/ProteinTranslator.java
+++ java/protein-translation/src/main/java/ProteinTranslator.java
@@ -1,8 +1,37 @@
 import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 class ProteinTranslator {
-
+    private static final Pattern CODONS = Pattern.compile(".{3}");
+    private final Map<String, String> mapOfRna = Map.ofEntries(
+            Map.entry("AUG", "Methionine"),
+            Map.entry("UUU", "Phenylalanine"),
+            Map.entry("UUC", "Phenylalanine"),
+            Map.entry("UUA", "Leucine"),
+            Map.entry("UUG", "Leucine"),
+            Map.entry("UCU", "Serine"),
+            Map.entry("UCC", "Serine"),
+            Map.entry("UCA", "Serine"),
+            Map.entry("UCG", "Serine"),
+            Map.entry("UAU", "Tyrosine"),
+            Map.entry("UAC", "Tyrosine"),
+            Map.entry("UGU", "Cysteine"),
+            Map.entry("UGC", "Cysteine"),
+            Map.entry("UGG", "Tryptophan"),
+            Map.entry("UAA", "STOP"),
+            Map.entry("UGA", "STOP"),
+            Map.entry("UAG", "STOP")
+    );
     List<String> translate(String rnaSequence) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return CODONS.matcher(rnaSequence)
+                .results()
+                .map(MatchResult::group)
+                .map(mapOfRna::get)
+                .takeWhile(Predicate.not("STOP"::equals))
+                .collect(Collectors.toList());
     }
 }
