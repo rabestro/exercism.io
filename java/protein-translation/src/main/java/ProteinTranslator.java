1bcad8da0c067052c4e944a4946ac0bf16873cbc
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-09-20 21:02:35 +0300

    Refined solution

--- java/protein-translation/src/main/java/ProteinTranslator.java
+++ java/protein-translation/src/main/java/ProteinTranslator.java
@@ -1,37 +1,45 @@
 import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
-import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import static java.util.function.Predicate.not;
+
 class ProteinTranslator {
-    private static final Pattern CODONS = Pattern.compile(".{3}");
-    private final Map<String, String> mapOfRna = Map.ofEntries(
-            Map.entry("AUG", "Methionine"),
-            Map.entry("UUU", "Phenylalanine"),
-            Map.entry("UUC", "Phenylalanine"),
-            Map.entry("UUA", "Leucine"),
-            Map.entry("UUG", "Leucine"),
-            Map.entry("UCU", "Serine"),
-            Map.entry("UCC", "Serine"),
-            Map.entry("UCA", "Serine"),
-            Map.entry("UCG", "Serine"),
-            Map.entry("UAU", "Tyrosine"),
-            Map.entry("UAC", "Tyrosine"),
-            Map.entry("UGU", "Cysteine"),
-            Map.entry("UGC", "Cysteine"),
-            Map.entry("UGG", "Tryptophan"),
-            Map.entry("UAA", "STOP"),
-            Map.entry("UGA", "STOP"),
-            Map.entry("UAG", "STOP")
-    );
+    private static final Pattern CODONS = Pattern.compile("(?<=\\G.{3})");
+
+    private String codonToProtein(String codon) {
+        switch (codon) {
+            case "AUG":
+                return "Methionine";
+            case "UUU":
+            case "UUC":
+                return "Phenylalanine";
+            case "UUA":
+            case "UUG":
+                return "Leucine";
+            case "UCU":
+            case "UCC":
+            case "UCA":
+            case "UCG":
+                return "Serine";
+            case "UAU":
+            case "UAC":
+                return "Tyrosine";
+            case "UGU":
+            case "UGC":
+                return "Cysteine";
+            case "UGG":
+                return "Tryptophan";
+            default:
+                return "STOP";
+        }
+    }
+
     List<String> translate(String rnaSequence) {
-        return CODONS.matcher(rnaSequence)
-                .results()
-                .map(MatchResult::group)
-                .map(mapOfRna::get)
-                .takeWhile(Predicate.not("STOP"::equals))
+        return CODONS.splitAsStream(rnaSequence)
+                .map(this::codonToProtein)
+                .takeWhile(not("STOP"::equals))
                 .collect(Collectors.toList());
     }
+
 }
