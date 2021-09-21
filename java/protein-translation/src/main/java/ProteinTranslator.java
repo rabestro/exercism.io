c13c4d6745153fe7a22338f9827c03a9a837d354
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-09-21 06:27:27 +0300

    Add Proverb

--- java/protein-translation/src/main/java/ProteinTranslator.java
+++ java/protein-translation/src/main/java/ProteinTranslator.java
@@ -1,6 +1,5 @@
 import java.util.List;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import static java.util.function.Predicate.not;
 
@@ -8,38 +7,23 @@ class ProteinTranslator {
     private static final Pattern CODONS = Pattern.compile("(?<=\\G.{3})");
 
     private String codonToProtein(String codon) {
-        switch (codon) {
-            case "AUG":
-                return "Methionine";
-            case "UUU":
-            case "UUC":
-                return "Phenylalanine";
-            case "UUA":
-            case "UUG":
-                return "Leucine";
-            case "UCU":
-            case "UCC":
-            case "UCA":
-            case "UCG":
-                return "Serine";
-            case "UAU":
-            case "UAC":
-                return "Tyrosine";
-            case "UGU":
-            case "UGC":
-                return "Cysteine";
-            case "UGG":
-                return "Tryptophan";
-            default:
-                return "STOP";
-        }
+        return switch (codon) {
+            case "AUG" -> "Methionine";
+            case "UUU", "UUC" -> "Phenylalanine";
+            case "UUA", "UUG" -> "Leucine";
+            case "UCU", "UCC", "UCA", "UCG" -> "Serine";
+            case "UAU", "UAC" -> "Tyrosine";
+            case "UGU", "UGC" -> "Cysteine";
+            case "UGG" -> "Tryptophan";
+            default -> "STOP";
+        };
     }
 
     List<String> translate(String rnaSequence) {
         return CODONS.splitAsStream(rnaSequence)
                 .map(this::codonToProtein)
                 .takeWhile(not("STOP"::equals))
-                .collect(Collectors.toList());
+                .toList();
     }
 
 }
