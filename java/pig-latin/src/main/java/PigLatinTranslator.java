61a8a7ed82c2c35be8eb22a84ddee5c1abd9e57d
Jegors Cemisovs <jegors.cemisovs@outlook.lv>
2021-10-04 20:37:22 +0300

    Rename constant

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,12 +1,12 @@
 import java.util.regex.Pattern;
 
 public final class PigLatinTranslator {
-    private static final Pattern RULES = Pattern.compile(
-            "(?<consonants>(?!xr|yt)y?((qu)|[\\w&&[^aeiouy]])*)?" +
-                    "(?<base>\\w+)");
+    private static final Pattern PATTERN = Pattern.compile(
+            "(?<consonants>(?!xr|yt)y?((qu)|[\\w&&[^aeiouy]])*)?"
+                    + "(?<base>\\w+)");
     private static final String TEMPLATE = "${base}${consonants}ay";
 
     public String translate(String sentence) {
-        return RULES.matcher(sentence).replaceAll(TEMPLATE);
+        return PATTERN.matcher(sentence).replaceAll(TEMPLATE);
     }
 }
