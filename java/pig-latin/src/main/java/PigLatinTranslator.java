2f3106d663ce69553cb96f0bbc227528de216419
Jegors Cemisovs <jegors.cemisovs@outlook.lv>
2021-10-04 20:39:00 +0300

    Update exercise PigLatinTranslator

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,12 +1,21 @@
 import java.util.regex.Pattern;
 
+import static java.util.stream.Collectors.joining;
+
 public final class PigLatinTranslator {
-    private static final Pattern PATTERN = Pattern.compile(
-            "(?<consonants>(?!xr|yt)y?((qu)|[\\w&&[^aeiouy]])*)?"
-                    + "(?<base>\\w+)");
-    private static final String TEMPLATE = "${base}${consonants}ay";
+    private static final Pattern WORDS_DELIMITER = Pattern.compile("\\s+");
+    private static final Pattern RULES = Pattern.compile(
+            "((?!yt|xr)(?<consonant>[^aeoui]?qu|[^aeoui][^aeouiy]*))?(?<body>.*)");
+    private static final String TEMPLATE = "${body}${consonant}ay";
 
     public String translate(String sentence) {
-        return PATTERN.matcher(sentence).replaceAll(TEMPLATE);
+        return WORDS_DELIMITER
+                .splitAsStream(sentence)
+                .map(this::translateWord)
+                .collect(joining(" "));
+    }
+
+    private String translateWord(String word) {
+        return RULES.matcher(word).replaceFirst(TEMPLATE);
     }
 }
