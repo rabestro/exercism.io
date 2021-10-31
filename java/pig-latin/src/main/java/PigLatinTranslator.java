5ef7de8e278683c1dcda6fce4c372f757feaef60
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2021-10-31 15:34:06 +0200

    Improve readability

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,9 +1,15 @@
 import java.util.regex.Pattern;
 
 public final class PigLatinTranslator {
-    private static final Pattern PATTERN = Pattern.compile("" +
-            "(?<consonants>(?!xr|yt)y?((qu)|[\\w&&[^aeiouy]])*)?" +
-            "(?<base>\\w+)");
+    private static final String CONSONANTS = "" +
+            "(?<consonants>" + // define named capture group
+            "(?!xr|yt)" + // consonants should not start with "xr" or "yt"
+            "y?((qu)|[\\w&&[^aeiouy]])*" + // the main definition of consonant group
+            ")?"; // The consonants group is optional. It is empty if word starts with vowel
+
+    private static final String BASE = "(?<base>\\w+)";
+
+    private static final Pattern PATTERN = Pattern.compile(CONSONANTS + BASE);
     private static final String TEMPLATE = "${base}${consonants}ay";
 
     public String translate(String sentence) {
