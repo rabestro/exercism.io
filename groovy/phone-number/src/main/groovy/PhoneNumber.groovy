0960649604b0dc43dfbfe6c73253d339d54ef97b
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2021-11-02 19:40:21 +0200

    Solved

--- groovy/phone-number/src/main/groovy/PhoneNumber.groovy
+++ groovy/phone-number/src/main/groovy/PhoneNumber.groovy
@@ -1,5 +1,12 @@
 class PhoneNumber {
+    static final PATTERN = ~/^\+?1|\D/
+
     static String clean(String input) {
-        throw new UnsupportedOperationException('method not implemented.')
+        def number = PATTERN.matcher(input).replaceAll('')
+
+        if (number.length() != 10 || number =~ /^[01]|^...[01]/) {
+            throw new NumberFormatException()
+        }
+        number
     }
 }
