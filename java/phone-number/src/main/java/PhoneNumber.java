c1630a1e29af669e1588a405b5a1895a2810b365
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2021-11-02 19:51:41 +0200

    Update code

--- java/phone-number/src/main/java/PhoneNumber.java
+++ java/phone-number/src/main/java/PhoneNumber.java
@@ -1,27 +1,28 @@
 import java.util.Map;
 
 class PhoneNumber {
+    private static final Map<String, String> ERRORS = Map.of(
+            ".*\\p{Alpha}.*", "letters not permitted",
+            ".*[@:!].*", "punctuations not permitted",
+            "\\d{12,}", "more than 11 digits",
+            "\\d{0,9}", "incorrect number of digits",
+            "[^1]\\d{10}", "11 digits must start with 1",
+            "1?0\\d{9}", "area code cannot start with zero",
+            "1?1\\d{9}", "area code cannot start with one",
+            "1?\\d{3}0\\d{6}", "exchange code cannot start with zero",
+            "1?\\d{3}1\\d{6}", "exchange code cannot start with one");
+
     private final String number;
 
     public PhoneNumber(String number) {
         var digits = number.replaceAll("[-+.() ]", "");
-        var errors = Map.of(
-                ".*\\p{Alpha}.*", "letters not permitted",
-                ".*[@:!].*", "punctuations not permitted",
-                "\\d{12,}", "more than 11 digits",
-                "\\d{0,9}", "incorrect number of digits",
-                "[^1]\\d{10}", "11 digits must start with 1",
-                "1?0\\d{9}", "area code cannot start with zero",
-                "1?1\\d{9}", "area code cannot start with one",
-                "1?\\d{3}0\\d{6}", "exchange code cannot start with zero",
-                "1?\\d{3}1\\d{6}", "exchange code cannot start with one");
 
-        for (var error : errors.entrySet()) {
+        for (var error : ERRORS.entrySet()) {
             if (digits.matches(error.getKey())) {
                 throw new IllegalArgumentException(error.getValue());
             }
         }
-        this.number = digits.length() == 11 ? digits.substring(1) : digits;
+        this.number = digits.substring(digits.length() - 10);
     }
 
     public String getNumber() {
