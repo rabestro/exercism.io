acbeb1d5727cacee5953dbea9426edce3e6f8784
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-02 14:22:42 +0200

    refactor regexp

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,8 +1,17 @@
 object PigLatin {
-    private val pattern = Regex("(?<consonants>(?!xr|yt)y?((qu)|[\\w&&[^aeiouy]])*)?(?<base>\\w+)")
+    private val pattern = Regex(
+        """
+        (?<consonants>                  # define named capture group for consonants
+            (?!xr|yt)                   # Rule 1: consonants should not start with "xr" or "yt"
+            y?                          # Rule 4: the letter 'y' is a consonant only at first position
+            ((qu)|[\w&&[^aeiouy]])*     # 
+        )?                              # the consonants group may be empty
+        (?<base>\w+)                    # part of a word after initial consonants
+        """.trimIndent(),
+        RegexOption.COMMENTS
+    )
 
-    fun translate(phrase: String): String {
-        return phrase.replace(pattern, "\${base}\${consonants}ay"
-        )
-    }
+    private const val template = "\${base}\${consonants}ay" // Rule 2
+
+    fun translate(phrase: String) = phrase.replace(pattern, template)
 }
