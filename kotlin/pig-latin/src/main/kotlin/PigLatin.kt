93680b74eaf48ddecd5ee47dfd8a6ee1aaec44ca
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-02 14:40:23 +0200

    add comments to regexp

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,17 +1,17 @@
 object PigLatin {
     private val pattern = Regex(
         """
-        (?<consonants>                  # define named capture group for consonants
+        (?<moved>                       # define named capturing group for moved letters
             (?!xr|yt)                   # Rule 1: consonants should not start with "xr" or "yt"
             y?                          # Rule 4: the letter 'y' is a consonant only at first position
-            ((qu)|[\w&&[^aeiouy]])*     # 
-        )?                              # the consonants group may be empty
+            (qu|[[a-z]&&[^aeiouy]])*    # Rule 3: word starts with a consonant sound followed by "qu"
+        )?                              # the moved group may be empty
         (?<base>\w+)                    # part of a word after initial consonants
         """.trimIndent(),
         RegexOption.COMMENTS
     )
 
-    private const val template = "\${base}\${consonants}ay" // Rule 2
+    private const val template = "\${base}\${moved}ay"
 
     fun translate(phrase: String) = phrase.replace(pattern, template)
 }
