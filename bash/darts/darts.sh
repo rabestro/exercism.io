829ec4e6d1be5ec09ae8119b9acc2be90d961b7f
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-02-08 23:46:45 +0200

    updated

--- bash/darts/darts.sh
+++ bash/darts/darts.sh
@@ -4,18 +4,9 @@ die () { echo "$1"; exit 1; }
 
 main () {
   (( $# != 2 )) && die "Invalid arg count"
-
   for i; do [[ $i = *[^[:digit:].-]* ]] && die "Non-numeric arg"; done
 
-  bc <<< "
-    x=$1
-    y=$2
-    d=x^2 + y^2
-
-    if (d > 100) 0
-    else if (d > 25) 1
-    else if (d > 1) 5
-    else 10"
+  bc <<< "d=$1^2 + $2^2; if (d > 100) 0 else if (d > 25) 1 else if (d > 1) 5 else 10"
 }
 
 main "$@"
