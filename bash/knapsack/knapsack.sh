bade4f0eb8c86a40a33309585b5781211f1c196f
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-02-11 16:08:58 +0200

    solved

--- bash/knapsack/knapsack.sh
+++ bash/knapsack/knapsack.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
-
-(($# == 0)) && exit 1
-
+#
+# Copyright (c) 2023 Jegors Čemisovs
+# License: Apache-2.0 license
+#
+# Knapsack problem solution
+#
+# Usage: knapsack.sh max_weight [items ...]
+# where: items look like "weight:value"
+# example:
+#       knapsack.sh 10 5:10 4:40 6:30 4:50
+# should return:
+#       90
+#
 arguments() {
+  (($# == 0)) && exit 1
+
   printf "%d\n" "$1"; shift
   printf "%d\n" $#
   while (($# > 0))
