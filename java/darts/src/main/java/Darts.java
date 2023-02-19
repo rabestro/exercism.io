8f7ab47da7f882b018e458cf03395efbee967621
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-02-19 12:26:03 +0200

    update Darts

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -1,23 +1,20 @@
-class Darts {
+record Darts() {
+    private static final int OUTER_CIRCLE = 100;
+    private static final int MIDDLE_CIRCLE = 25;
+    private static final int INNER_CIRCLE = 1;
 
-    private final double distance;
+    int score(double x, double y) {
+        var distance = x * x + y * y;
 
-    Darts(double x, double y) {
-        this.distance = x * x + y * y;
-    }
-
-    int score() {
-
-        if (distance > 100) {
+        if (distance > OUTER_CIRCLE) {
             return 0;
         }
-        if (distance > 25) {
+        if (distance > MIDDLE_CIRCLE) {
             return 1;
         }
-        if (distance > 1) {
+        if (distance > INNER_CIRCLE) {
             return 5;
         }
         return 10;
     }
-
 }
