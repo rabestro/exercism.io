8e962e73ef53175217cbedcde1ee469079a47ac5
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-02-23 13:52:33 +0200

    Scale the coordinates

--- javascript/coordinate-transformation/coordinate-transformation.js
+++ javascript/coordinate-transformation/coordinate-transformation.js
@@ -15,7 +15,9 @@
  *  translated coordinate pair in the form [x, y]
  */
 export function translate2d(dx, dy) {
-  return (x, y) => {return x + dx, y + dy};
+    return (x, y) => {
+        return [x + dx, y + dy]
+    };
 }
 
 /**
@@ -29,7 +31,9 @@ export function translate2d(dx, dy) {
  *  scaled coordinate pair in the form [x, y]
  */
 export function scale2d(sx, sy) {
-  throw new Error('Implement the scale2d function');
+    return function (x, y) {
+        return [x * sx, y * sy];
+    };
 }
 
 /**
@@ -43,7 +47,7 @@ export function scale2d(sx, sy) {
  *  transformed coordinate pair in the form [x, y]
  */
 export function composeTransform(f, g) {
-  throw new Error('Implement the composeTransform function');
+    throw new Error('Implement the composeTransform function');
 }
 
 /**
@@ -56,5 +60,5 @@ export function composeTransform(f, g) {
  *  if the arguments are the same on subsequent calls, or compute a new result if they are different.
  */
 export function memoizeTransform(f) {
-  throw new Error('Implement the memoizeTransform function');
+    throw new Error('Implement the memoizeTransform function');
 }
