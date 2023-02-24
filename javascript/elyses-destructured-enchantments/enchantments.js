aa88d9ff93882cfcff1783ce4338b820e3c0d254
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-02-24 09:44:48 +0200

    add enchantments.js

--- javascript/elyses-destructured-enchantments/enchantments.js
+++ javascript/elyses-destructured-enchantments/enchantments.js
@@ -62,5 +62,6 @@ const FACE_CARDS = ['jack', 'queen', 'king'];
  * third, and fourth cards are the face cards
  */
 export function insertFaceCards(deck) {
-    throw new Error('Implement the insertFaceCards function');
+    const [first, ...rest] = deck;
+    return [first, ...FACE_CARDS, ...rest];
 }
