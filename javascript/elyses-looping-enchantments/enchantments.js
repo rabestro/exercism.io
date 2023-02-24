e26124cf8afa22ce86f78d7961599f05076b28a8
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-02-24 20:40:30 +0200

    1. Determine how many cards of a certain type are in the deck

--- javascript/elyses-looping-enchantments/enchantments.js
+++ javascript/elyses-looping-enchantments/enchantments.js
@@ -9,7 +9,11 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  throw new Error('Implement the cardTypeCheck function');
+    let count = 0;
+    stack.forEach(x => {
+        if (x === card) ++count
+    });
+    return count
 }
 
 /**
@@ -20,5 +24,5 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  throw new Error('Implement the determineOddEvenCards function');
+    throw new Error('Implement the determineOddEvenCards function');
 }
