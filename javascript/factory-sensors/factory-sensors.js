90007f78471b7e9016d173b82577fcf04ce63031
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-02-24 17:42:50 +0200

    3. Monitor the machine

--- javascript/factory-sensors/factory-sensors.js
+++ javascript/factory-sensors/factory-sensors.js
@@ -43,5 +43,16 @@ export function reportOverheating(temperature) {
  * @throws {ArgumentError|OverheatingError|Error}
  */
 export function monitorTheMachine(actions) {
-    throw new Error('Implement the monitorTheMachine function');
+    try {
+        actions.check();
+    } catch (error) {
+        if (error instanceof ArgumentError) {
+            actions.alertDeadSensor();
+            return
+        }
+        if (error instanceof OverheatingError)
+            if (error.temperature < 600) actions.alertOverheating()
+            else actions.shutdown()
+        else throw error;
+    }
 }
