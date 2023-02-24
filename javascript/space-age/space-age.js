a749d387ebfb39396d2ffeb65e6140fa0ecd866c
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-02-24 20:57:46 +0200

    update space-age.js

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,16 +1,21 @@
+const EARTH_YEAR_SECOND = 3.15576e7;
+
+const ORBITAL_PERIODS = {
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+}
+const FORMAT = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 2 });
+
 export const age = (planet, seconds) => {
-    const orbitalPeriods = {
-        earth: 1,
-        mercury: 0.2408467,
-        venus: 0.61519726,
-        mars: 1.8808158,
-        jupiter: 11.862615,
-        saturn: 29.447498,
-        uranus: 84.016846,
-        neptune: 164.79132
-    }
-    const ageOnEarth = seconds / 3.15576e7;
-    return round(ageOnEarth / orbitalPeriods[planet]);
+    const ageOnEarth = seconds / EARTH_YEAR_SECOND;
+    const ageOnPlanet = ageOnEarth / ORBITAL_PERIODS[planet];
+    return FORMAT.format(ageOnPlanet);
 };
 
 function round(number) {
