e56e00212801dc05052a989708c09c7c671c2baa
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-02-27 18:43:56 +0200

    update pangram.js

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,3 +1,18 @@
-export const isPangram = phrase =>
-  !'abcdefghijklmnopqrstuvwxyz'
-      .replaceAll(new RegExp(`[${phrase}]`, 'gi'), '');
+// Benchmark: https://jsben.ch/oOHKq
+
+const A_LCASE = 97;
+const A_UCASE = 65;
+const Z_LCASE = 122;
+const Z_UCASE = 90;
+const ALL_26_BITS_SET = 67108863;
+
+export function isPangram(phrase) {
+    let set = 0;
+    for (let i = phrase.length; i-- > 0;) {
+        const c = phrase.charCodeAt(i);
+        if (c >= A_UCASE && c <= Z_UCASE) set |= 1 << c - A_UCASE;
+        else if (c >= A_LCASE && c <= Z_LCASE) set |= 1 << c - A_LCASE;
+        if (set === ALL_26_BITS_SET) return true;
+    }
+    return false;
+}
