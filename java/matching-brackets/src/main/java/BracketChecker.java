43455c28945f850d5294199fdd03452fb1bdc2f6
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-03-08 20:51:08 +0200

    add BracketChecker

---
+++ java/matching-brackets/src/main/java/BracketChecker.java
@@ -0,0 +1,12 @@
+final class BracketChecker {
+    private final String expression;
+
+    BracketChecker(String expression) {
+        this.expression = expression;
+    }
+
+    public boolean areBracketsMatchedAndNestedCorrectly() {
+
+        return true;
+    }
+}
