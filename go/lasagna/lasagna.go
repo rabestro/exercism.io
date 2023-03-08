a67a6360979ba4714a7b8ed6366d8e2eabf52948
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-03-08 09:12:18 +0200

    4. Calculate the elapsed working time in minutes

--- go/lasagna/lasagna.go
+++ go/lasagna/lasagna.go
@@ -9,10 +9,10 @@ func RemainingOvenTime(actualMinutesInOven int) int {
 
 // PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
 func PreparationTime(numberOfLayers int) int {
-	panic("PreparationTime not implemented")
+	return 2 * numberOfLayers
 }
 
 // ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
 func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
-	panic("ElapsedTime not implemented")
+	return PreparationTime(numberOfLayers) + actualMinutesInOven
 }
