d867758275728abcfcf8835098a14c227f536ff6
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-03-08 09:10:25 +0200

    2. Calculate the remaining oven time in minutes

--- go/lasagna/lasagna.go
+++ go/lasagna/lasagna.go
@@ -4,7 +4,7 @@ const OvenTime = 40
 
 // RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
 func RemainingOvenTime(actualMinutesInOven int) int {
-	panic("RemainingOvenTime not implemented")
+	return OvenTime - actualMinutesInOven
 }
 
 // PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
