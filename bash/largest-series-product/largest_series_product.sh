46f9f830178a761128323de31207eabd2e3257bb
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-03-17 00:11:45 +0200

    solved

--- bash/largest-series-product/largest_series_product.sh
+++ bash/largest-series-product/largest_series_product.sh
@@ -1,41 +1,32 @@
 #!/usr/bin/env bash
 
 die () {
-  echo "$1"
-  exit 1
+    echo "$1"; exit 1
 }
 one () {
-  echo 1
-  exit
+    echo 1; exit
 }
 
 (( $# == 1 )) || (( $2 == 0 )) && one
 
-readonly number="$1"
-readonly span="$2"
+readonly number="$1" span="$2"
 
 (( ${#number} < $2 )) && die "span must be smaller than string length"
-[[ $1 = *[^[:digit:]]* ]] && die "input must only contain digits"
+[[ $1 == *[^[:digit:]]* ]] && die "input must only contain digits"
 (( span < 0 )) && die "span must not be negative"
 
-bc <<< "
-number = $number
-span = $span
+declare -i max_index="${#number} - $span + 1" product max_product=0 digit
 
-define digit(i) {
-    return number % 10^i / 10^--i
-}
+for (( i = 0; i < max_index; ++i ))
+do
+    digit=${number:${i}:1}
+    (( product = digit ))
+    for (( k = 1; k < span; ++k ))
+    do
+       digit=${number:$((i + k)):1}
+       (( product *= digit ))
+    done
+    (( max_product = product > max_product ? product : max_product ))
+done
 
-max_index = length(number) - span + 1
-
-for (i = 1; i <= max_index; ++i) {
-    product = digit(i)
-    for (k = 1; k < span; ++k) {
-        product *= digit(i + k)
-    }
-    if (product > max_product) {
-        max_product = product
-    }
-}
-
-print max_product"
+echo $max_product
