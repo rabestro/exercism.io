66eb2cd696bafa7084028cc23a1bdf34ba0f6a0f
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-04-05 17:57:48 +0300

    Add minimum code to pass the first test

--- java/bowling/src/main/java/BowlingGame.java
+++ java/bowling/src/main/java/BowlingGame.java
@@ -1,10 +1,10 @@
-/*
+class BowlingGame {
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    public void roll(int pins) {
 
-Please remove this comment when submitting your solution.
+    }
 
-*/
+    public int score() {
+        return 0;
+    }
+}
