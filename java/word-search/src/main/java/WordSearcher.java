b279d2286221ffa9766c90ef270d611e8901eec5
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-04-23 18:21:18 +0300

    add Square

--- java/word-search/src/main/java/WordSearcher.java
+++ java/word-search/src/main/java/WordSearcher.java
@@ -1,9 +1,13 @@
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 class WordSearcher {
     Map<String, Optional<WordLocation>> search(final Set<String> words, final char[][] grid) {
-        throw new UnsupportedOperationException("Please implement the WordSearcher.search() method.");
+        var square = new Square(grid);
+        return words.stream()
+                .collect(Collectors.toUnmodifiableMap(Function.identity(), square::wordLocation));
     }
 }
