db7ac2cc5ac9d401076af6e96a012c0c77673b60
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-07-18 23:16:54 +0300

    solved SpiralMatrixBuilder

--- java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
+++ java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
@@ -1,10 +1,29 @@
-/*
+class SpiralMatrixBuilder {
+    private static final int[][] DELTA = {
+            {0, 1}, // right
+            {1, 0}, // down
+            {0, -1}, // left
+            {-1, 0} // up
+    };
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    int[][] buildMatrixOfSize(int size) {
+        var matrix = new int[size][size];
+        var row = 0;
+        var col = 0;
+        var direction = 0;
 
-Please remove this comment when submitting your solution.
+        for (var i = 1; i <= size * size; i++) {
+            matrix[row][col] = i;
+            var nextRow = row + DELTA[direction][0];
+            var nextCol = col + DELTA[direction][1];
+            var isOutOfBounds = nextRow < 0 || nextRow >= size || nextCol < 0 || nextCol >= size;
+            if (isOutOfBounds || matrix[nextRow][nextCol] != 0) {
+                direction = (direction + 1) % 4;
+            }
+            row += DELTA[direction][0];
+            col += DELTA[direction][1];
+        }
+        return matrix;
+    }
 
-*/
+}
