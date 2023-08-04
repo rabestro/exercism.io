fc1e4b7f988c17c4c4395a3bc127634db1eb3586
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-04 21:55:56 +0300

    Refactor clock.sh for better input validation
    Improved robustness of the clock.sh script by refactoring the validate_parameters function. The update ensures that an error is only thrown when the number of arguments is not 2 or 4. Additionally, the refactoring includes checks to confirm that the input for hours and minutes are integers. This change simplifies the error handling and enhances the efficiency of the script.

--- bash/clock/clock.sh
+++ bash/clock/clock.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 
-die () { echo "$1"; exit 1; }
-
-print_clock () {
-    local -ir MINUTES_IN_DAY=60*24
-    local -ir delta="$3$4"
-    local -ir total_minutes=$(( MINUTES_IN_DAY + $1 % 24 * 60 + $2 % MINUTES_IN_DAY + delta % MINUTES_IN_DAY ))
-    local -ir hours='total_minutes / 60 % 24'
-    local -ir minutes='total_minutes % 60'
-    printf "%02d:%02d\n" $hours $minutes
-}
+die () { echo "$1" >&2; exit 1; }
 
 validate_parameters () {
     (( $# == 2 || $# == 4 )) || die "invalid arguments"
-    [[ $1 =~ ^-?[0-9]+$ ]] || die "Hours must be an integer"
-    [[ $2 =~ ^-?[0-9]+$ ]] || die "Minutes must be an integer"
+    [[ $1 =~ ^-?[[:digit:]]+$ ]] || die "Hours must be an integer"
+    [[ $2 =~ ^-?[[:digit:]]+$ ]] || die "Minutes must be an integer"
     [[ $3 =~ ^[+-]$ ]] || [ -z "$3" ] || die "invalid arguments"
-    [[ $4 =~ ^[0-9]+$ ]] || [ -z "$4" ] || die "Delta must be an integer"
+    [[ $4 =~ ^[[:digit:]]+$ ]] || [ -z "$4" ] || die "Delta must be an integer"
+}
+
+print_clock () {
+    local -ir MINUTES_IN_DAY=1440
+    local -ir delta=$3$4
+    local -ir total_minutes=$(( MINUTES_IN_DAY + $1 % 24 * 60 + $2 % MINUTES_IN_DAY + delta % MINUTES_IN_DAY ))
+    local -ir hours=$(( total_minutes / 60 % 24 ))
+    local -ir minutes=$(( total_minutes % 60 ))
+    printf "%02d:%02d\n" $hours $minutes
 }
 
 validate_parameters "$@"
-
 print_clock "$@"
