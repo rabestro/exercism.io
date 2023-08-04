6fbcae256cf690723a7d7f6ba66720b8432a9610
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-04 22:42:24 +0300

    Add movement functionality to robot simulator
    This commit introduces a loop implementation that interprets a string of characters as movement commands for the robot. It processes 'A' as step forward in the current direction, 'R' as turning right, and 'L' as turning left. This increases the script's functionality significantly by enabling more complex robot movements.

--- bash/robot-simulator/robot_simulator.sh
+++ bash/robot-simulator/robot_simulator.sh
@@ -4,6 +4,30 @@ main () {
     local -i x=$1
     local -i y=$2
     local direction=${3:-north}
+    local movements=$4
+
+    for (( i=0; i<${#movements}; i++ )); do
+        case ${movements:$i:1} in
+            'A') case $direction in
+                    'north') ((y++)) ;;
+                    'east') ((x++)) ;;
+                    'south') ((y--)) ;;
+                    'west') ((x--)) ;;
+                  esac ;;
+            'R') case $direction in
+                    'north') direction=east ;;
+                    'east') direction=south ;;
+                    'south') direction=west ;;
+                    'west') direction=north ;;
+                  esac ;;
+            'L') case $direction in
+                    'north') direction=west ;;
+                    'east') direction=north ;;
+                    'south') direction=east ;;
+                    'west') direction=south ;;
+                  esac ;;
+        esac
+    done
 
     echo $x $y $direction
 }
