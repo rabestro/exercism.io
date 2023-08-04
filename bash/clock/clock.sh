e86cba326a6666b241615ca3224c4be8e3b9b525
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-04 21:20:18 +0300

    Improve time calculation in clock script
    This commit changes the way how time is calculated in the clock script. It fixes the calculation of total_minutes to correctly handle negative inputs for hours and minutes. It defines how to format negative time as time before midnight. Also, it includes a newline character in the output, which improves readability.

--- bash/clock/clock.sh
+++ bash/clock/clock.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 main () {
-    local -ir MINUTES_IN_DAY=$(( 60 * 24 ))
+    local -ir MINUTES_IN_DAY=60*24
     local -ir delta="$3$4"
     local -ir total_minutes=$(( MINUTES_IN_DAY + $1 % 24 * 60 + $2 % MINUTES_IN_DAY + delta % MINUTES_IN_DAY ))
-    local -ir hours=$(( total_minutes / 60 % 24 ))
-    local -ir minutes=$(( total_minutes % 60 ))
+    local -ir hours='total_minutes / 60 % 24'
+    local -ir minutes='total_minutes % 60'
     printf "%02d:%02d\n" $hours $minutes
 }
 
