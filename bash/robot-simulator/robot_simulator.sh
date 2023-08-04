002217346bda35287392b6f625ee8b16027ca7b7
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-04 23:04:56 +0300

    Refactor direction assignments in robot simulator script
    The direction assignments in the robot simulator script have been simplified and made more readable by removing the quotation marks around the direction strings. This refactor does not change the functionality of the script. Also, the local integer variables were initialized in one line to reduce verbosity and make code cleaner.

--- bash/robot-simulator/robot_simulator.sh
+++ bash/robot-simulator/robot_simulator.sh
@@ -3,8 +3,7 @@
 die () { echo "$1" >&2; exit 1; }
 
 main () {
-    local -i x=$1
-    local -i y=$2
+    local -i x=$1 y=$2
     local direction=${3:-north}
     local movements=$4
 
@@ -14,22 +13,22 @@ main () {
     for (( i=0; i<${#movements}; i++ )); do
         case ${movements:$i:1} in
             'A') case $direction in
-                    'north') ((y++)) ;;
-                    'east') ((x++)) ;;
-                    'south') ((y--)) ;;
-                    'west') ((x--)) ;;
+                    north) ((y++)) ;;
+                    east) ((x++)) ;;
+                    south) ((y--)) ;;
+                    west) ((x--)) ;;
                   esac ;;
             'R') case $direction in
-                    'north') direction=east ;;
-                    'east') direction=south ;;
-                    'south') direction=west ;;
-                    'west') direction=north ;;
+                    north) direction=east ;;
+                    east) direction=south ;;
+                    south) direction=west ;;
+                    west) direction=north ;;
                   esac ;;
             'L') case $direction in
-                    'north') direction=west ;;
-                    'east') direction=north ;;
-                    'south') direction=east ;;
-                    'west') direction=south ;;
+                    north) direction=west ;;
+                    east) direction=north ;;
+                    south) direction=east ;;
+                    west) direction=south ;;
                   esac ;;
         esac
     done
