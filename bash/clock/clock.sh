36be0e12f870db4634c401b4d0cbc69b7144606e
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-04 20:46:26 +0300

    Fix negative time calculation in clock
    Clock script was previously unable to correctly interpret negative hours and minutes inputs. Updated the total_minutes calculation to accommodate for negative timing by interpreting them as time before midnight. Also updated the output to include a newline character for clean formatting.

--- bash/clock/clock.sh
+++ bash/clock/clock.sh
@@ -1,12 +1,12 @@
 
 main () {
-    local -i total_minutes=$(( $1*60 + $2 ))
+    local -i total_minutes=$(( $1 % 24 * 60 + $2 ))
     if (( total_minutes < 0 )); then
-        (( total_minutes = 60 * 24 + total_minutes % 60 ))
+        (( total_minutes = 60 * 24 + total_minutes ))
     fi
     local -ir hours=$(( total_minutes / 60 % 24 ))
     local -ir minutes=$(( total_minutes % 60 ))
-    printf "%02d:%02d" $hours $minutes
+    printf "%02d:%02d\n" $hours $minutes
 }
 
 main "$@"
