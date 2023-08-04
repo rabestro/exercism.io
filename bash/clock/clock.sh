b81b2e0882cddb3f294720baaa20a3b13ce586f2
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-04 21:35:47 +0300

    Refactor clock script, add validation and error handling
    This commit improves the clock.sh script by refactoring the main function into print_clock and die functions which print the clock and handle errors respectively. It ensures that inputs for hours and minutes are integers and also handles invalid arguments efficiently by displaying an informative error message. The feature enhances code readability and provides better error handling.

--- bash/clock/clock.sh
+++ bash/clock/clock.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-main () {
+die () { echo "$1"; exit 1; }
+
+print_clock () {
     local -ir MINUTES_IN_DAY=60*24
     local -ir delta="$3$4"
     local -ir total_minutes=$(( MINUTES_IN_DAY + $1 % 24 * 60 + $2 % MINUTES_IN_DAY + delta % MINUTES_IN_DAY ))
@@ -9,4 +11,14 @@ main () {
     printf "%02d:%02d\n" $hours $minutes
 }
 
-main "$@"
+validate_parameters () {
+    [ $# -ne 2 ] && [ $# -ne 4 ] && die "invalid arguments"
+    [[ $1 =~ ^-?[0-9]+$ ]] || die "Hours must be an integer"
+    [[ $2 =~ ^-?[0-9]+$ ]] || die "Minutes must be an integer"
+    [[ $3 =~ ^[+-]$ ]] || [ -z "$3" ] || die "invalid arguments"
+    [[ $4 =~ ^[0-9]+$ ]] || [ -z "$4" ] || die "Delta must be an integer"
+}
+
+validate_parameters "$@"
+
+print_clock "$@"
