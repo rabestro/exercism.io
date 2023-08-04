4da49825b360d85a7dc7734bd7f0071a9fb2c2de
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-04 20:25:30 +0300

    Correct negative time input in clock script
    Updated the clock.sh script to handle negative input for time. Previously, the script would fail when provided negative values for hours or minutes due to the `local -ir` flag which restricts reassigning of values. The logic now interprets negative time as time before midnight and calculates the corresponding clock time.

--- bash/clock/clock.sh
+++ bash/clock/clock.sh
@@ -1,6 +1,9 @@
 
 main () {
-    local -ir total_minutes=$(( $1*60 + $2 ))
+    local -i total_minutes=$(( $1*60 + $2 ))
+    if (( total_minutes < 0 )); then
+        (( total_minutes = 60 * 24 + total_minutes % 60 ))
+    fi
     local -ir hours=$(( total_minutes / 60 % 24 ))
     local -ir minutes=$(( total_minutes % 60 ))
     printf "%02d:%02d" $hours $minutes
