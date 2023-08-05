374fe4dd4726005631557679ee60c392c9e6560f
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-05 16:36:55 +0300

    "Refactor error handling and simplify scoring logic in bowling.sh
    Introduced an associative array to manage error messages for a more standardized way of handling errors and simplifying the logic. This change also leverages variables for strike and spare cases in scoring, making the calculations more intuitive and traceable. These amendments enhanced clarity and readability of the code."

--- bash/bowling/bowling.sh
+++ bash/bowling/bowling.sh
@@ -3,21 +3,26 @@
 declare -ar THROWS=("$@")
 declare -ir MAX_PINS=10
 declare -ir LAST_FRAME=10
+declare -A ERROR_MESSAGE=(
+    [incomplete]="Score cannot be taken until the end of the game"
+    [negative]="Negative roll is invalid"
+    [exceeds]="Pin count exceeds pins on the lane"
+    [game_over]="Cannot roll after game is over"
+)
 
 die () {
-    echo "$1" >&2
-    exit 1
+    echo "${ERROR_MESSAGE[$1]:-$1}" >&2; exit 1
 }
 
 validate_pins () {
     local -ir pins=$1
-    (( pins < 0 )) && die "Negative roll is invalid."
-    (( pins > MAX_PINS )) && die "Pin count exceeds pins on the lane."
+    (( pins < 0 )) && die negative
+    (( pins > MAX_PINS )) && die exceeds
 }
 
 get_pins () {
     local -ir throw=$1
-    (( throw >= ${#THROWS[*]} )) && die "Score cannot be taken until the end of the game."
+    (( throw >= ${#THROWS[*]} )) && die incomplete
 
     local -ir pins=${THROWS[$throw]}
     validate_pins $pins
@@ -52,9 +57,9 @@ score () {
         fi
         (( strike )) && (( throw-- ))
     done
-    (( frame == 10 )) || die "Score cannot be taken until the end of the game."
+    (( frame == 10 )) || die incomplete
     (( throw += spare + 2*strike ))
-    (( throw == ${#THROWS[*]} )) || die "Cannot roll after game is over."
+    (( throw == ${#THROWS[*]} )) || die game_over
     echo $score
 }
 
