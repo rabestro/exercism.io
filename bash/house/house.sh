23dcc5d1bf06345ced5792d6f15d9400940acdeb
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-05 19:53:20 +0300

    Update house.sh with expanded nursery rhyme verses
    Extended the initial simple 'house' script into a more complex version where the nursery rhyme is built up verse by verse. This expansion allows for a dynamic build-up of the nursery rhyme "This is the house that Jack built" by declaring arrays of subjects and verbs, then using recursive function to form each verse. Included are also error checks for invalid input arguments.

--- bash/house/house.sh
+++ bash/house/house.sh
@@ -1,3 +1,46 @@
 #!/usr/bin/env bash
 
-echo "This is the house that Jack built."
+declare -ar subj=(
+    house malt rat cat dog
+    'cow with the crumpled horn'
+    'maiden all forlorn'
+    'man all tattered and torn'
+    'priest all shaven and shorn'
+    'rooster that crowed in the morn'
+    'farmer sowing his corn'
+    'horse and the hound and the horn'
+)
+declare -ar verb=(
+    'lay in' ate killed worried tossed milked
+    kissed married woke kept 'belonged to'
+)
+declare -ir max_verse=${#subj[@]}
+
+phrase () {
+    local -i verse=$1
+    echo "the ${subj[$verse]}"
+    (( verse-- )) && echo "that ${verb[$verse]} $(phrase $verse)"
+}
+
+nursery_rhyme () {
+    local -ir verse=$(( $1 - 1 ))
+    echo "This is $(phrase $verse) that Jack built."
+    echo ''
+}
+
+main() {
+    local -ir from=$1 to=$2
+    local -i verse
+
+    if (( from < 1 || from > max_verse || to < from || to > max_verse )); then
+        echo "invalid arguments"
+        exit 1
+    fi
+
+    for ((verse=$from; verse<=$to; verse++))
+    do
+        nursery_rhyme $verse
+    done
+}
+
+main "$@"
