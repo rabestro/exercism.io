f073256cd9ab31d61c33230e3485296a0cc8a9f5
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-05 22:51:53 +0300

    Update Go dependencies and test cases
    Upgraded the Go dependencies from 1.13 to 1.18 for the roman-numerals and darts exercises. Modified the existing test cases in the `cases_test.go` files for both exercises for updated problem specifications. Also, optimised the darts test case by adding a condition to skip benchmarks in short mode.

--- go/darts/darts_test.go
+++ go/darts/darts_test.go
@@ -9,14 +9,16 @@ func TestScore(t *testing.T) {
 		t.Run(tc.description, func(t *testing.T) {
 			actual := Score(tc.x, tc.y)
 			if actual != tc.expected {
-				t.Errorf("Score(%#v, %#v) = %#v, want: %#v",
-					tc.x, tc.y, actual, tc.expected)
+				t.Errorf("Score(%#v, %#v) = %#v, want: %#v", tc.x, tc.y, actual, tc.expected)
 			}
 		})
 	}
 }
 
 func BenchmarkScore(b *testing.B) {
+	if testing.Short() {
+		b.Skip("skipping benchmark in short mode.")
+	}
 	for i := 0; i < b.N; i++ {
 		for _, tc := range testCases {
 			Score(tc.x, tc.y)
