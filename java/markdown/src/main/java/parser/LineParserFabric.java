97708464f2b3d88fc1ea97bf2f2c46643c0dcbe0
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-06 17:37:18 +0300

    Refactor code to delete ParagraphParser class
    Removed the ParagraphParser and incorporated its logic directly into LineParserFabric to streamline the parser selection process. This helps in reducing abstraction layers and makes the codebase easier to maintain, enhancing efficiency and readability.

--- java/markdown/src/main/java/parser/LineParserFabric.java
+++ java/markdown/src/main/java/parser/LineParserFabric.java
@@ -1,12 +1,13 @@
 package parser;
 
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class LineParserFabric implements Function<String, Function<String, String>> {
     private static final Parser LIST_PARSER = new ReplaceAllParser("^\\* (.+)$", "<li>$1</li>");
+    private static final Parser PARAGRAPH_PARSER = new ReplaceAllParser("^.*$", "<p>$0</p>");
 
     private final Parser headerParser = new HeaderParser();
-    private final Parser paragraphParser = new ParagraphParser();
     private final Parser textStyleParser = new TextStylesParser();
 
     @Override
@@ -17,7 +18,7 @@ public class LineParserFabric implements Function<String, Function<String, Strin
         } else if (isListItem(line)) {
             lineParser = LIST_PARSER;
         } else {
-            lineParser = paragraphParser;
+            lineParser = PARAGRAPH_PARSER;
         }
         return lineParser.andThen(textStyleParser);
     }
