65630eac698092f538bc78f8fa1c18815863868f
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-06 17:31:06 +0300

    Remove redundant matchers from LineParserFabric
    Redundant predicates were removed from LineParserFabric to simplify its logic. The various matchers were no longer needed due to the introduction of the Parser interface and the refactoring of related classes. This has resulted in more maintainable code and a centralized parser selection logic, improving code organization and readability.

--- java/markdown/src/main/java/parser/LineParserFabric.java
+++ java/markdown/src/main/java/parser/LineParserFabric.java
@@ -1,14 +1,8 @@
 package parser;
 
 import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.regex.Pattern;
 
 public class LineParserFabric implements Function<String, Function<String, String>> {
-    private static final Predicate<String> LIST_ITEM_MATCHER = Pattern.compile("(<li>).*").asMatchPredicate();
-    private static final Predicate<String> HEADER_MATCHER = Pattern.compile("(<h).*").asMatchPredicate();
-    private static final Predicate<String> PARAGRAPH_MATCHER = Pattern.compile("(<p>).*").asMatchPredicate();
-
     private final Parser headerParser = new HeaderParser();
     private final Parser listItemParser = new ListItemParser();
     private final Parser paragraphParser = new ParagraphParser();
