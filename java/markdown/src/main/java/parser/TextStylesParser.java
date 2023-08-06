b3cd3b7cc06954dca78de70e1d706fd9a6c48b69
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-06 16:21:05 +0300

    Refactor TextStylesParser for better performance
    Refactored TextStylesParser to use dependency injection inside constructor instead of locally creating new instances of ReplaceAllParser, leading to improved performance. Also, this change simplifies the 'apply' method by using java.util.function.Function - makes the design significantly more flexible for future modifications and testing.

--- java/markdown/src/main/java/parser/TextStylesParser.java
+++ java/markdown/src/main/java/parser/TextStylesParser.java
@@ -1,21 +1,20 @@
 package parser;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
-public final class TextStylesParser implements UnaryOperator<String> {
-    private static final List<ReplaceAllParser> PARSERS = List.of(
-            new ReplaceAllParser("__(.+)__", "<strong>$1</strong>"),
-            new ReplaceAllParser("_(.+)_", "<em>$1</em>")
-    );
+public record TextStylesParser(List<Function<String, String>> parsers) implements UnaryOperator<String> {
+    public TextStylesParser() {
+        this(List.of(
+                new ReplaceAllParser("__(.+)__", "<strong>$1</strong>"),
+                new ReplaceAllParser("_(.+)_", "<em>$1</em>")));
+    }
 
     @Override
     public String apply(String text) {
-        var result = Optional.ofNullable(text);
-        for (var parser : PARSERS) {
-            result = result.map(parser);
-        }
-        return result.orElse(text);
+        return parsers().stream()
+                .reduce(Function.identity(), Function::andThen)
+                .apply(text);
     }
 }
