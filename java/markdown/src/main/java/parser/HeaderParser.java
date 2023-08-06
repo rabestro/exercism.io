c8f234b8d9269c224578e01efec962547a193d77
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-06 13:24:56 +0300

    Refactor HeaderParser to use dependency injection
    The HeaderParser class has been refactored to use dependency injection in the constructor. This reduces instantiations of LeadingHashesCounter, HeaderParserBuilder and TextStylesParser in the apply method. This improves performance by reducing unnecessary object creation and makes the design more flexible for testing and future modifications.

--- java/markdown/src/main/java/parser/HeaderParser.java
+++ java/markdown/src/main/java/parser/HeaderParser.java
@@ -1,13 +1,23 @@
 package parser;
 
+import java.util.function.IntFunction;
+import java.util.function.ToIntFunction;
 import java.util.function.UnaryOperator;
 
-public final class HeaderParser implements UnaryOperator<String> {
+public record HeaderParser(
+        ToIntFunction<String> leadingHashesCounter,
+        IntFunction<UnaryOperator<String>> headerParserBuilder,
+        UnaryOperator<String> textStylesParser) implements UnaryOperator<String> {
+
+    HeaderParser() {
+        this(new LeadingHashesCounter(), new HeaderParserBuilder(), new TextStylesParser());
+    }
+
 
     @Override
     public String apply(String text) {
-        var headerLevel = new LeadingHashesCounter().applyAsInt(text);
-        var headerParser = new HeaderParserBuilder().apply(headerLevel);
-        return headerParser.andThen(new TextStylesParser()).apply(text);
+        var headerLevel = leadingHashesCounter.applyAsInt(text);
+        var headerParser = headerParserBuilder.apply(headerLevel);
+        return headerParser.andThen(textStylesParser).apply(text);
     }
 }
