9b3e5d18e8ec96d30471a0fd560c84a3d62fadaf
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-07 21:35:23 +0300

    Refactored ForthEvaluator operations to use Map and enabled extensibility
    Updated the evaluateOperation method in ForthEvaluator.java to maintain operations in a map, which simplifies the method itself and makes this method more extendable for future addition of operations. The newly created forthCore map holds the operations and corresponding lambda expressions to perform task. The 'macros' map has been added to initialize these operations.
    
    Additionally, some methods were rearranged for readability purposes in ForthEvaluator.java, and the 'over' operation that duplicates the second to last stack value was implemented.

--- java/forth/src/main/java/ForthEvaluator.java
+++ java/forth/src/main/java/ForthEvaluator.java
@@ -1,10 +1,63 @@
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class ForthEvaluator {
+
     private final Deque<Integer> stack = new ArrayDeque<>();
+    private final Map<String, Runnable> forthCore = Map.of(
+            "+", () -> {
+                requireStackHasAtLeast(2, "Addition requires that the stack contain at least 2 values");
+                stack.add(stack.removeLast() + stack.removeLast());
+            },
+            "-", () -> {
+                requireStackHasAtLeast(2, "Subtraction requires that the stack contain at least 2 values");
+                stack.add(-stack.removeLast() + stack.removeLast());
+            },
+            "*", () -> {
+                requireStackHasAtLeast(2, "Multiplication requires that the stack contain at least 2 values");
+                stack.add(stack.removeLast() * stack.removeLast());
+            },
+            "/", () -> {
+                requireStackHasAtLeast(2, "Division requires that the stack contain at least 2 values");
+                int divisor = stack.removeLast();
+                int dividend = stack.removeLast();
+                if (divisor == 0) {
+                    throw new IllegalArgumentException("Division by 0 is not allowed");
+                }
+                stack.add(dividend / divisor);
+            },
+            "dup", () -> {
+                requireStackHasAtLeast(1, "Duplicating requires that the stack contain at least 1 value");
+                stack.add(stack.peekLast());
+            },
+            "drop", () -> {
+                requireStackHasAtLeast(1, "Dropping requires that the stack contain at least 1 value");
+                stack.removeLast();
+            },
+            "swap", () -> {
+                requireStackHasAtLeast(2, "Swapping requires that the stack contain at least 2 values");
+                int a = stack.removeLast();
+                int b = stack.removeLast();
+                stack.add(a);
+                stack.add(b);
+            },
+            "over", () -> {
+                requireStackHasAtLeast(2, "Overing requires that the stack contain at least 2 values");
+                int a = stack.removeLast();
+                int b = stack.getLast();
+                stack.add(a);
+                stack.add(b);
+            }
+    );
+    private final Map<String, Runnable> macros;
+
+    ForthEvaluator() {
+        this.macros = new HashMap<>(forthCore);
+    }
 
     public List<Integer> evaluateProgram(List<String> commands) {
         commands.forEach(this::evaluateLine);
@@ -24,51 +77,9 @@ class ForthEvaluator {
     }
 
     private void evaluateOperation(String token) {
-        switch (token) {
-            case "+" -> {
-                requireStackHasAtLeast(2, "Addition requires that the stack contain at least 2 values");
-                stack.add(stack.removeLast() + stack.removeLast());
-            }
-            case "-" -> {
-                requireStackHasAtLeast(2, "Subtraction requires that the stack contain at least 2 values");
-                stack.add(-stack.removeLast() + stack.removeLast());
-            }
-            case "*" -> {
-                requireStackHasAtLeast(2, "Multiplication requires that the stack contain at least 2 values");
-                stack.add(stack.removeLast() * stack.removeLast());
-            }
-            case "/" -> {
-                requireStackHasAtLeast(2, "Division requires that the stack contain at least 2 values");
-                int divisor = stack.removeLast();
-                int dividend = stack.removeLast();
-                if (divisor == 0) {
-                    throw new IllegalArgumentException("Division by 0 is not allowed");
-                }
-                stack.add(dividend / divisor);
-            }
-            case "dup" -> {
-                requireStackHasAtLeast(1, "Duplicating requires that the stack contain at least 1 value");
-                stack.add(stack.peekLast());
-            }
-            case "drop" -> {
-                requireStackHasAtLeast(1, "Dropping requires that the stack contain at least 1 value");
-                stack.removeLast();
-            }
-            case "swap" -> {
-                requireStackHasAtLeast(2, "Swapping requires that the stack contain at least 2 values");
-                int a = stack.removeLast();
-                int b = stack.removeLast();
-                stack.add(a);
-                stack.add(b);
-            }
-            case "over" -> {
-                requireStackHasAtLeast(2, "Overing requires that the stack contain at least 2 values");
-                int a = stack.removeLast();
-                int b = stack.getLast();
-                stack.add(a);
-                stack.add(b);
-            }
-        }
+        macros.getOrDefault(token, () -> {
+            throw new IllegalArgumentException("Unknown operation");
+        }).run();
     }
 
     private void requireStackHasAtLeast(int n, String message) {
