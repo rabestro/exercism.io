de444e26ffb931079d819f5ffa8884bbd3b74e68
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-07 16:27:03 +0300

    Implement basic ForthEvaluator functionality
    Added basic functionality to ForthEvaluator class. Imported necessary classes and implemented foundational methods like evaluateProgram, evaluateLine, evaluateToken, evaluateOperation, and isNumber. Focused on setting up a token parsing and validation framework with future commands and operations in mind. Implemented a stack mechanism to handle token processing. Future enhancements will add advanced capabilities to the operations.

--- java/forth/src/main/java/ForthEvaluator.java
+++ java/forth/src/main/java/ForthEvaluator.java
@@ -1,8 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Queue;
 
 class ForthEvaluator {
+    private final Queue<Integer> stack = new ArrayDeque<>();
 
     public List<Integer> evaluateProgram(List<String> commands) {
-        return List.of(1, 2, 3, 4, 5);
+        commands.forEach(this::evaluateLine);
+        return stack.stream().toList();
+    }
+
+    private void evaluateLine(String line) {
+        Arrays.asList(line.split(" ")).forEach(this::evaluateToken);
+    }
+
+    private void evaluateToken(String token) {
+        if (isNumber(token)) {
+            stack.add(Integer.parseInt(token));
+        } else {
+            evaluateOperation(token);
+        }
+    }
+
+    private void evaluateOperation(String token) {
+
+    }
+
+    private boolean isNumber(String token) {
+        return true;
     }
 }
