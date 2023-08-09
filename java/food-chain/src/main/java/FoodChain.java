6da759eba9abc5d31125605dba896ada167d7712
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-09 22:14:28 +0300

    Add logic to 'verse' method in FoodChain.java
    Added the logic to the 'verse' method in FoodChain class to return the string verse form given an integer input. Introduced two array constants ANIMALS and VERSES to store the animals and the verses of the FoodChain song respectively. This enables us to pull the correct verse based on the input integer. The 'verses' method remains to be developed.

--- java/food-chain/src/main/java/FoodChain.java
+++ java/food-chain/src/main/java/FoodChain.java
@@ -1,7 +1,32 @@
+import java.util.StringJoiner;
+
 class FoodChain {
+    private static final String[] ANIMALS = {
+            "fly",
+            "spider",
+            "bird",
+            "cat",
+            "dog",
+            "goat",
+            "cow",
+            "horse"
+    };
+    private static final String[] VERSES = {
+            "I don't know why she swallowed the fly. Perhaps she'll die.",
+            "It wriggled and jiggled and tickled inside her.",
+            "How absurd to swallow a bird!",
+            "Imagine that, to swallow a cat!",
+            "What a hog, to swallow a dog!",
+            "Just opened her throat and swallowed a goat!",
+            "I don't know how she swallowed a cow!",
+            "She's dead, of course!"
+    };
 
     public String verse(int verse) {
-        return null;
+        var sb = new StringJoiner(System.lineSeparator());
+        sb.add("I know an old lady who swallowed a " + ANIMALS[verse - 1] + ".");
+        sb.add(VERSES[verse - 1]);
+        return sb.toString();
     }
 
     public String verses(int startVerse, int endVerse) {
