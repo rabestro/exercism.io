5fd8c90053d2f81a41f8525c85672db03230859d
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-12 10:08:44 +0300

    Add validation checks to base conversion script and adjust test cases
    The base conversion script, all_your_base.sh was revised to add validation checks for the input and output bases and each digit in the input number in the convert_base function. An error message is returned if the base or digit is incorrect. The main function was also renamed to convert_base for more clarity. In addition, bats test cases were unskipped to enable robust testing of the revised script. This refactor makes the script more maintainable and prevents incorrect inputs from running.

--- bash/all-your-base/all_your_base.sh
+++ bash/all-your-base/all_your_base.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
 
-main() {
+die () { echo "$1" >&2 ; exit 1; }
+
+convert_base() {
   local -ir source_base="$1"
   local -ir target_base="$3"
-  local -ar digits=($2)
+  local -a digits=($2)
   local -i power="${#digits[@]}"
   local -i number=0
   local -i digit
 
-  for digit in "${digits[@]}"; do
-    (( number += source_base ** --power * digit ))
-  done
-  echo "$number"
+  (( source_base > 1 )) || die "input base is incorrect"
+  (( target_base > 1 )) || die "output base is incorrect"
 
+  for digit in "${digits[@]}"; do
+    (( digit >= 0 && digit < source_base )) || die "digit is incorrect"
+    ((number += source_base ** --power * digit))
+  done
+
+  ((number)) && digits=() || digits=(0)
+
+  while ((number)); do
+    (( digit = number % target_base ))
+    (( number /= target_base ))
+    digits=("$digit" "${digits[@]}")
+  done
+  
+  echo "${digits[@]}"
 }
 
-main "$@"
+convert_base "$@"
