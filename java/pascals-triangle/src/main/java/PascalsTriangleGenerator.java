fe0174925abc7647b01d87fac2c41a95fd1bf760
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-12 19:51:42 +0300

    Add logic to generate Pascal's triangle
    The function `generateTriangle` in the `PascalsTriangleGenerator` class has been updated to include the logic necessary to generate Pascal's triangle up to the given level. The implemented logic generates the triangle row by row from top to bottom, initializing the first and last element of each row as 1, and calculating the remaining values based on the previous row. This implementation returns the complete Pascal's triangle as a 2-dimensional array.
    
    Simultaneously, test cases in 'PascalsTriangleGeneratorTest' had @Ignore annotations removed to enable the tests as these can now be executed successfully with the new logic. This was done to validate the implemented logic and ensure it works as expected.

--- java/pascals-triangle/src/main/java/PascalsTriangleGenerator.java
+++ java/pascals-triangle/src/main/java/PascalsTriangleGenerator.java
@@ -1,6 +1,15 @@
 class PascalsTriangleGenerator {
 
     public int[][] generateTriangle(int level) {
-        return new int[][]{};
+        var triangle = new int[level][];
+        for (int i = 0; i < level; i++) {
+            triangle[i] = new int[i + 1];
+            triangle[i][0] = 1;
+            triangle[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+        return triangle;
     }
 }
