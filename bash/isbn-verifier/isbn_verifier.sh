f0e9cb64b3c413214169762b484e573a6ef0a1dd
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-12 22:47:19 +0300

    Refactor isbn_verifier.sh for brevity and clarity
    The script was simplified by making use of bash parameter expansion for arithmetic computations, removing unnecessary comments and redundant code, and organizing the conditions for better readability. The verification logic remained unchanged, ensuring the script still correctly verifies ISBN-10.

--- bash/isbn-verifier/isbn_verifier.sh
+++ bash/isbn-verifier/isbn_verifier.sh
@@ -1,62 +1,22 @@
 #!/usr/bin/env bash
 
-#Instructions
-#
-#The ISBN-10 verification process is used to validate book identification numbers. These normally contain dashes and look like: 3-598-21508-8
-#ISBN
-#
-#The ISBN-10 format is 9 digits (0 to 9) plus one check character (either a digit or an X only). In the case the check character is an X, this represents the value '10'. These may be communicated with or without hyphens, and can be checked for their validity by the following formula:
-#
-#(d₁ * 10 + d₂ * 9 + d₃ * 8 + d₄ * 7 + d₅ * 6 + d₆ * 5 + d₇ * 4 + d₈ * 3 + d₉ * 2 + d₁₀ * 1) mod 11 == 0
-#
-#If the result is 0, then it is a valid ISBN-10, otherwise it is invalid.
-#Example
-#
-#Let's take the ISBN-10 3-598-21508-8. We plug it in to the formula, and get:
-#
-#(3 * 10 + 5 * 9 + 9 * 8 + 8 * 7 + 2 * 6 + 1 * 5 + 5 * 4 + 0 * 3 + 8 * 2 + 8 * 1) mod 11 == 0
-#
-#Since the result is 0, this proves that our ISBN is valid.
-#Task
-#
-#Given a string the program should check if the provided string is a valid ISBN-10. Putting this into place requires some thinking about preprocessing/parsing of the string prior to calculating the check digit for the ISBN.
-#
-#The program should be able to verify ISBN-10 both with and without separating dashes.
-#Caveats
-#
-#Converting from strings to numbers can be tricky in certain languages. Now, it's even trickier since the check digit of an ISBN-10 may be 'X' (representing '10'). For instance 3-598-21507-X is a valid ISBN-10.
-
 false() {
-    echo "false"
-    exit 0
+  echo "false"
+  exit 0
 }
 
-main () {
-    local isbn=${1//-/}
-    local sum=0
-    local i=10
-    local digit
+main() {
+  local isbn=${1//-/}
+  [[ $isbn =~ ^[[:digit:]]{9}[[:digit:]X]$ ]] || false
 
-    [[ $isbn =~ ^[0-9]{9}[0-9X]$ ]] || false
+  local -i sum=$((${isbn: -1} == "X" ? 10 : ${isbn: -1}))
+  local -i i=10
+  while ((i > 1)); do
+    ((sum += ${isbn: -i:1} * i--))
+  done
 
-    if [[ ${#isbn} -ne 10 ]]; then
-        echo "false"
-        exit 0
-    fi
-
-    for digit in $(echo $isbn | grep -o .); do
-        if [[ $digit == "X" ]]; then
-            digit=10
-        fi
-        sum=$((sum + digit * i))
-        ((i--))
-    done
-
-    if [[ $((sum % 11)) -eq 0 ]]; then
-        echo "true"
-    else
-        echo "false"
-    fi
+  ((sum % 11)) && false
+  echo "true"
 }
 
 main "$@"
